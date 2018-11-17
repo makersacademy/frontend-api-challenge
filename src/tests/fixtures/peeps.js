03c88048cafdebe5e3014d4536df98d5c384357f
Todd Langford-Archer <toddpla@gmail.com>
2018-11-17 20:54:43 +0000

    amended test fixtures

--- src/tests/fixtures/peeps.js
+++ src/tests/fixtures/peeps.js
@@ -1,9 +1,33 @@
 export default [
   {
-    body: "test peep one"
+    body: "test peep one",
+    id: 1,
+    likes: [],
+    created_at: "2018-11-17T19:40:58.497Z",
+    updated_at: "2018-11-17T19:40:58.497Z",
+    user: {
+      handle: "toddpla",
+      id: 100
+    }
   }, {
-    body: "test peep two"
+    body: "test peep two",
+    id: 1,
+    likes: [],
+    created_at: "2018-11-17T19:40:58.497Z",
+    updated_at: "2018-11-17T19:40:58.497Z",
+    user: {
+      handle: "toddpla",
+      id: 100
+    }
   }, {
-    body: "test peep three"
+    body: "test peep three",
+    id: 1,
+    likes: [],
+    created_at: "2018-11-17T19:40:58.497Z",
+    updated_at: "2018-11-17T19:40:58.497Z",
+    user: {
+      handle: "toddpla",
+      id: 100
+    }
   }
 ]
