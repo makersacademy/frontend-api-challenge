19cf6d9883559fa37019f834cf186c184aedb611
sacullezzar <lucas.razzell@holidayextras.com>
2018-11-18 22:34:17 +0000

    working on random peep generator

--- src/interface.js
+++ src/interface.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var button = document.getElementById('showAllPeeps')
+  var allbutton = document.getElementById('showAllPeeps')
+  var randbutton = document.getElementById('randomPeep')
+  var randPeep = document.getElementById('randPeep')
   var allPeeps = document.getElementById('allPeeps')
-  button.addEventListener("click", function () {
+
+  allbutton.addEventListener("click", function () {
     if( allPeeps.style.display == "none" ) {
       allPeeps.style.display = "block"
     } else {
       allPeeps.style.display = "none"
     }
-    listAllPeeps()
+    listAllPeeps(url)
+  })
+
+  randbutton.addEventListener("click", function () {
+    if (randPeep.style.display == "none") {
+      randPeep.style.display = "block"
+    } else {
+      randPeep.style.display = "none"
+    }
+    randomPeep()
   })
 })
