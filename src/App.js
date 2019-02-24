9428b3d25137dbc0bfe9e0ef63d3f53e814a5770
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-24 22:36:45 +0000

    add feature to display peeps in app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component, Fragment } from "react";
 import Logo from "./components/Logo";
+import Peep from "./components/Peep";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    peeps: [],
+    loggedIn: false
+  };
+
+  componentWillMount() {
+    const getPeeps = async () => {
+      const endpoint = "https://chitter-backend-api.herokuapp.com/peeps";
+      const response = await fetch(endpoint);
+      const peepsJson = await response.json();
+      this.setState({ peeps: peepsJson });
+    };
+    getPeeps();
+  }
+
+  
+
   render() {
     return (
       <Fragment>
         <Logo />
+        {this.state.peeps.map(peep => (
+          <Peep key={peep.id} details={peep} />
+        ))}
+        {console.log(this.state.peeps)}
       </Fragment>
     );
   }
