f4d68527dc8858c869873a71c4b7d248ccf5da21
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-24 17:00:15 +0000

    add Logo component

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import "../App.css";
 
 class Logo extends React.Component {
@@ -7,9 +7,7 @@ class Logo extends React.Component {
     return (
       <Fragment>
         <div className="chitter__logo">
-          {/* <Link to="/"> */}
           <h1>Chitter</h1>
-          {/* </Link> */}
         </div>
       </Fragment>
     );
