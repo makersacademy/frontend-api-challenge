621e2515064752de8534189b210d4a42ac5c9b12
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-24 10:19:30 +0000

    New component: Peeps. App.js is now rendering Peeps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Peeps from './components/Peeps.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <p>Peeps</p>
+      <Peeps />
       </div>
     );
   }
---
+++ src/components/Peeps.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Peeps extends Component {
+  render() {
+    return (
+      <div className="App">
+      <p>Peeps</p>
+      </div>
+    );
+  }
+}
+
+export default Peeps;
