225d25d975ef12db43aae169428f85d0b1e07ad8
Simon Green <simon.green@thielsen.co.uk>
2019-02-24 15:35:40 +0000

    Clean up of directory

--- src/router.js
+++ src/router.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import Users from './views/Users.vue'
 import Sessions from './views/Sessions.vue'
 import Peeps from './views/Peeps.vue'
 import Peep from './views/Peep.vue'
-import Likes from './views/Likes.vue'
 
 Vue.use(Router)
 
@@ -14,14 +12,6 @@ export default new Router({
     {
       path: '/',
       redirect: '/peeps'
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       path: '/users',
@@ -42,11 +32,6 @@ export default new Router({
       path: '/peeps',
       name: 'peeps',
       component: Peeps
-    },
-    {
-      path: 'peeps/:peep_id/likes/:user_id',
-      name: 'likes',
-      component: Likes
     }
   ]
 })
