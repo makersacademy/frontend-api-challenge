502fc27e56b814db556adfe59c5ee43c352d65bb
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-24 10:52:37 +0000

    displaying peeps and time of creation

---
+++ src/peeps-list.js
@@ -0,0 +1,48 @@
+var peepsListContainer = document.getElementById('peeps-list');
+var loginButton = document.getElementById('login');
+var postForm = document.getElementById('post-form');
+var newPeep = document.getElementById('create-peep');
+
+//
+
+window.onload = () => {
+  renderPeeps();
+  loginOrSignupOrPost();
+};
+
+//when user clicks on login button
+loginButton.addEventListener('click', () => {
+  window.location.href="./login.html"; //when the user clicks login button - load login html
+})
+
+async function renderPeeps(){
+  var apiPeeps = await fetchPeepsFromAPI();
+  wrapAllPeepsInHTML(apiPeeps);
+};
+
+function fetchPeepsFromAPI(url = 'https://chitter-backend-api.herokuapp.com/peeps'){
+  return fetch(url)
+  .then((allAPIPeepsRawData) => {
+    return allAPIPeepsRawData.json();
+  }).then((allPeepsJSONData) => {
+      return allPeepsJSONData;
+    }).catch((err) => {
+      console.log('Oops, Something went Wrong. Try again!', err); //fetch promises only gives network (failed request)errors so we have to manual construct a a type error for this function
+    });
+};
+
+function wrapAllPeepsInHTML(JSONData){
+  var returnedHTMLString = '';
+  var hideDeleteBetweenText = '';
+  var hideLikesBetweenText = '';
+
+  for (var i = 0; i < Object.keys(JSONData).length; i ++ ) {
+    returnedHTMLString += `${JSONData[i].user.handle}: ${(JSONData[i].body)} @${JSONData[i].created_at.slice(11,16)} on ${JSONData[i].created_at.slice(0,10)}\n `
+  }
+  console.log(returnedHTMLString)
+}
+
+function loginOrSignupOrPost(){
+
+
+}
