36554d3dfce3dcb9417d8258b9df3afd534a108d
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-21 13:38:15 +0100

    Delegated parsing info to chitterAPI. Expanded on data displayed

---
+++ src/chitterAPI.js
@@ -0,0 +1,17 @@
+class chitterAPI {
+  constructor() {
+    this.url = 'https://chitter-backend-api.herokuapp.com';
+    // our api resource
+  }
+
+  // peeps => given attributes at chitter api backend page
+  renderPeeps() {
+    $.get(`${this.url}/peeps`, function(responseData) {
+      $(responseData).each(function() {
+        $('#listPeeps').append(`<li class="peep" id="${this.id}">${this.body}<br>${this.user.handle} <br>${this.created_at.substr(11,5)} ${this.created_at.substr(0,10)}</li>`)
+      })
+    })
+  }
+  // substring implemented to get preferred form. reference from chitter api backend page
+
+}
--- src/interface.js
+++ src/interface.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
-   $('#getPeeps').on('click', function() {
-    var url = 'https://chitter-backend-api.herokuapp.com/peeps'; // given api
-    $.get(url, function(data) {
-      $('#listPeeps').text(data.map(peep => peep.body));
-    })
-  });
+  const chitter = new chitterAPI();
 
- })
+  $('#getPeeps').click(function() {
+    chitter.renderPeeps() // delegate to chitterAPI
+  })
+
+})
