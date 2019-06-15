a2cc82d4cc37c445f9d1a382e590f211eb9b8842
Megan Goode <megan.e.goode@hotmail.com>
2019-06-15 17:57:43 +0100

    can create a session for the user

--- spec/userSpec.js
+++ spec/userSpec.js
@@ -23,6 +23,30 @@ describe("User", function() {
       expect(valueId).toEqual(1);
       expect(valueHandle).toEqual('handle');
     });
+
+    it('can create a session for the user', function() {
+      var user = new User();
+
+      var userId, sessionKey;
+      user = {
+        session: function(handle, password) {
+          userId = 1;
+          sessionKey = "qwerty";
+        },
+        getId: function() {
+          return userId;
+        },
+        getKey: function() {
+          return sessionKey;
+        }
+      }
+
+      spyOn(user, 'getId');
+      spyOn(user, 'getKey');
+      user.session('handle', 'pass123');
+      expect(userId).toEqual(1);
+      expect(sessionKey).toEqual('qwerty');
+    });
   });
 
 });
