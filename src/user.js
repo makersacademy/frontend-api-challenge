932900466292b8973926edefe39b536a3e913f17
Megan Goode <megan.e.goode@hotmail.com>
2019-06-15 18:54:17 +0100

    Add session key post

--- src/user.js
+++ src/user.js
@@ -1,6 +1,7 @@
-function User(id, handle) {
+function User(id, handle, sessionKey) {
  this._id = id;
  this._handle = handle;
+ this._sessionKey = sessionKey;
 }
 
 User.prototype.create = function(handle, password, callback) {
@@ -19,7 +20,24 @@ User.prototype.create = function(handle, password, callback) {
     success: function(result) {
       this._id = result.id;
       this._handle = result.handle;
+      this.session(this._handle, password)
       callback(this._handle);
+    }
+  });
+}
+
+User.prototype.session = function(handle, password, callback) {
+  $.ajax({
+    url: "https://chitter-backend-api.herokuapp.com/sessions",
+    type: "POST",
+    data: {
+      session: {
+        handle: handle,
+        password: password
+      }
+    },
+    success: function(result) {
+      this._sessionKey = result.session_key;
     }
   });
 }
