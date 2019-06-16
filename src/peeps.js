ab2e8509bf44aaf4e62a438ed21d171fdf894eae
Taj <tajbi21@gmail.com>
2019-06-16 01:37:44 +0100

    can see all peeps

--- src/peeps.js
+++ src/peeps.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $.get('https://chitter-backend-api.herokuapp.com/peeps', function(data) {
     data.forEach(function(element) {
-      $(".peeps").append("<li>" + element.body + "</li>");
+      $(".peep-container").append("<p>" + "user: " + element.user["handle"] + " " + element.body + " " + element.created_at +"<p>");
     });
 
-    
+// {"id":828,"body":"Final test.","created_at":"2019-06-15T23:41:43.288Z","updated_at":"2019-06-15T23:41:43.288Z","user":{"id":1144,"handle":"meg1704"},"likes":[]}
+
   });
 
   // $(document).ready(function(){
