5a3659494bd78a6e4af6e6fdd73b6e256d3eebc1
Anthony Hull <m@anthonyhull.me>
2019-06-16 23:01:27 +0100

    refactored to logic out of html file. now displays multiple peeps

--- cypress/integration/viewPeeps.spec.js
+++ cypress/integration/viewPeeps.spec.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe('Mainpage', () => {
   beforeEach(function () {
     // "this" points at the test context object
@@ -12,12 +14,27 @@ describe('Mainpage', () => {
 
 
   // the test callback is in "function () { ... }" form
-  it('can get peeps from the API', function () {
+  it('can recieve a non empty reply from the API', function () {
     // this.user exists
     cy.visit('/')
     cy.wait('@peeps').then((xhr) => {
       assert.isNotNull(xhr.response.body.data, 'list peeps API call has data')
     })
   })
+
+  it('can display a peep from the API', function () {
+    // this.user exists
+    cy.visit('/')
+    cy.wait('@peeps')
+    cy.contains("And I'd like to take a minute Just sit right there I'll tell you how I became the prince of a town called Bel Air")
+  })
+
+  it('can display multiple peeps from the API', function () {
+    // this.user exists
+    cy.visit('/')
+    cy.wait('@peeps')
+    cy.contains("And I'd like to take a minute Just sit right there I'll tell you how I became the prince of a town called Bel Air")
+  })
+
   // expect(this.peeps[0].body).to.equal("And I'd like to take a minute Just sit right there I'll tell you how I became the prince of a town called Bel Air")
 })
