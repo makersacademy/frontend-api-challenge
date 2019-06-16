83789c59bb910fa97162ef43ad32e791a1e7c0a5
edwardreeve <edreeve+git@gmail.com>
2019-06-16 22:30:38 +0100

    sorted weird login thing (element IDs weren't unique)

--- js/controller.js
+++ js/controller.js
@@ -13,26 +13,33 @@ $(document).ready(function () {
 
   $("#login-form").submit(function (event) {
     event.preventDefault(); 
-    var handle = $("input#username").val();
-    var password = $("input#password").val();
+    var handle = $("input#loginUsername").val();
+    var password = $("input#loginPassword").val();
     chitter.loginUser(handle, password);
-    
   });
 
   
   $("#register-form").submit(function (event) {
     event.preventDefault(); 
-    var handle = $("input#username").val();
-    var password = $("input#password").val();
+    var handle = $("input#registerUsername").val();
+    var password = $("input#registerPassword").val();
     chitter.register(handle, password);
+    setTimeout(function () { chitter.loginUser(handle, password); }, 10);
   });
-
+  
   $("#logout").click(function () {
     $("#banner-and-nav-logged-in").attr('hidden', 'true');
     $("#banner-and-nav-register").removeAttr('hidden');
+    $("#post").attr('hidden', 'true');
     chitter.user_id = null;
     chitter.username = null;
     chitter.session_id = null;
+  });
+  
+  $("#peepForm").submit(function (event) {
+    event.preventDefault(); 
+    var message = $("input#message-text").val();
+    chitter.post(chitter.user_id, message, chitter.session_id, chitter.createFeed())
   });
 
       // Leave the following one - it's the whole thing
--- src/Chitter.js
+++ src/Chitter.js
@@ -28,13 +28,14 @@ Chitter.prototype.loginUser = function(handle, password) {
   $this = this
   var Data;
   request = $.ajax({
-    url: 'https://chitter-backend-api.herokuapp.com/sessions',
     contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    url: 'https://chitter-backend-api.herokuapp.com/sessions',
     type: 'POST',
     data: `{"session": {"handle":"${handle}", "password":"${password}"}}`,
-    dataType: "json",
     success: function (response) {
       Data = response;
+      $("#post").removeAttr('hidden');
     },
     statusCode: {
       422: function () {
@@ -46,6 +47,7 @@ Chitter.prototype.loginUser = function(handle, password) {
       201: function () {
         $("#banner-and-nav-logged-in").removeAttr('hidden');
         $("#banner-and-nav-register").attr('hidden', 'true');
+        $('#loginModal').modal('toggle');
       },
       error: function (e) {
         alert("Server error - " + e);
@@ -59,7 +61,7 @@ Chitter.prototype.loginUser = function(handle, password) {
   });  
 }
 
-Chitter.prototype.register = function(handle, password) {
+Chitter.prototype.register = function(handle, password, callback) {
   $.ajax({
     url: 'https://chitter-backend-api.herokuapp.com/users',
     contentType: "application/json; charset=utf-8",
@@ -79,6 +81,33 @@ Chitter.prototype.register = function(handle, password) {
         alert("Server error - " + e);
       }
     }
-  })
+  });
+  if (typeof callback == "function")
+    callback();
+}
+
+Chitter.prototype.post = function(user_id, message, session_key, callback) {
+  $.ajax({
+    url: 'https://chitter-backend-api.herokuapp.com/peeps',
+    headers: `Authorization: Token token=${session_key}`,
+    contentType: "application/json; charset=utf-8",
+    type: 'POST',
+    data: `{"peep": {"user_id":${user_id}, "body":"${message}"}}`,
+    dataType: "json",
+    statusCode: {
+      500: function () {
+        alert('Summat went wrong - 500 error')
+      },
+      200: function () {
+        alert('whoop! 200')
+        $('#newPeep').modal('toggle');
+      },
+      error: function (e) {
+        alert("Server error - " + e);
+      }
+    }
+  });
+  if (typeof callback == "function")
+    callback();
 }
 
