d903aca9b18307c8c97ce027fd24e303c0b9d611
Taj <tajbi21@gmail.com>
2019-06-17 13:32:40 +0100

    user can create account

--- src/user.js
+++ src/user.js
@@ -1,15 +1,37 @@
 $(document).ready(function(){
 
-  var $handle = $('#username');
-  var $password = $('#password');
-  var $newSession = '';
-  var $peep = $('#peep')
+  var handle = $('#username');
+  var password = $('#password');
+  var newSession = '';
+  var userId = '';
+  var peep = $('#peep')
 
+  //create new user
+  $('#register').on('click', function() {
+    console.log("hello")
+    var newAccount = {
+      user: {
+      handle: handle.val(),
+      password: password.val()
+      }
+    }
+    $.ajax({
+      type:'POST',
+      url: 'https://chitter-backend-api.herokuapp.com/users',
+      data: newAccount,
+      success: function(newUser) {
+        alert("You are now registered on chitter, please login to peep");
+        console.log(newUser);
+      }
+    });
+  });
+
+  // user can log in, username taj485 and password taj4855
   $('#login').on('click',function() {
     var data = {
       session: {
-        handle: $handle.val(),
-        password: $password.val(),
+        handle: handle.val(),
+        password: password.val(),
       }
     }
 
@@ -17,30 +39,34 @@ $(document).ready(function(){
       type:'POST',
       url:'https://chitter-backend-api.herokuapp.com/sessions/',
       data: data,
-      success: function(newSession) {
-        $newSession = newSession.session_key;
+      success: function(result) {
+        newSession = result.session_key;
+        userId = result.user_id;
       },
       error: function() {
         alert('error logging in')
       }
     });
+    console.log(userId);
   });
 
+  //user can send peeps once logged in
+  $('#submitPeep').on('click', function() {
+    var newPeep = {
+      peep: {
+        user_id: 1200,
+        body: peep.val(),
+      }
+    }
 
+    $.ajax({
+      type: 'POST',
+      url: 'https://chitter-backend-api.herokuapp.com/peeps',
+      headers:{'Authorization': 'Token token=' + newSession},
+      data: newPeep,
+      success: function(result) {
+        console.log(result);
+      }
+    });
+  });
 });
-// User.prototype.session = function(name,password) {
-//   this.sessionId =
-//
-//   $.post('https://chitter-backend-api.herokuapp.com/sessions', function(session) {
-//     console.log(session)
-//     });
-//
-// }
-//
-//
-// https://chitter-backend-api.herokuapp.com/users
-// {"session": {"handle":"sarah123", "password":"sarah123"}}
-//
-// https://chitter-backend-api.herokuapp
-// .com/sessions
-// {"user_id":1145,"session_key":"_2a_10_jJWvTTQxis9w9ZAdCsn0QO"}
