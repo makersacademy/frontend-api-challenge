acaf27d7e96854cff4ea24e85e6282113a7aabc5
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-01 19:29:29 +0000

    user can sign up...but sees no confirmation

--- src/getpeeps.js
+++ src/getpeeps.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
       return response.json();
     }).then(function(data) {
 
-      $.each(data, function (index, value) {
+      $.each(data, function(index, value) {
         var node1 = document.createElement("P");
         var node2 = document.createElement("P");
         var node3 = document.createElement("P");
         var br = document.createElement("BR");
 
         var text = document.createTextNode(value.body);
-        var time = document.createTextNode("Written on: "+ value.created_at.substring(0, 10));
+        var time = document.createTextNode("Written on: " + value.created_at.substring(0, 10));
         var author = document.createTextNode("Authored by: " + value.user.handle);
 
         node1.appendChild(text);
@@ -30,5 +30,30 @@ $(document).ready(function() {
     });
 
   });
+
+  document.getElementById('signup').addEventListener('submit', signup);
+
+  function signup(event) {
+    event.preventDefault();
+
+    fetch('https://chitter-backend-api.herokuapp.com/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "user": {
+            handle: document.getElementById('handle').value,
+            password: document.getElementById('password').value
+          }
+        })
+      })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(JSON.stringify(data));
+      });
+  };
 
 });
