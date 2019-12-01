ee635beae44d40a54affcae2fe0b46ca4df3b64c
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-01 15:50:14 +0000

    click the button to see the peeps

--- src/getpeeps.js
+++ src/getpeeps.js
@@ -1,30 +1,34 @@
-fetch('https://chitter-backend-api.herokuapp.com/peeps').then(function(response) {
-  return response.json();
-}).then(function(data) {
+$(document).ready(function() {
 
-  $.each(data, function (index, value) {
-    var node = document.createElement("P");
-    var text = document.createTextNode(value.body);
-    var time = document.createTextNode(value.created_at);
-    var br = document.createElement("BR");
-    node.appendChild(text);
-    node.appendChild(br);
-    node.appendChild(time);
+  $('#see').on('click', function() {
 
-    document.getElementById('main').appendChild(node);
-    console.log(value);
+    fetch('https://chitter-backend-api.herokuapp.com/peeps').then(function(response) {
+      return response.json();
+    }).then(function(data) {
 
+      $.each(data, function (index, value) {
+        var node1 = document.createElement("P");
+        var node2 = document.createElement("P");
+        var node3 = document.createElement("P");
+        var br = document.createElement("BR");
 
-    $.each(this, function (index, value) {
-      // second loop
+        var text = document.createTextNode(value.body);
+        var time = document.createTextNode("Written on: "+ value.created_at.substring(0, 10));
+        var author = document.createTextNode("Authored by: " + value.user.handle);
+
+        node1.appendChild(text);
+        node2.appendChild(time);
+        node3.appendChild(author);
+
+        document.getElementById('main').appendChild(node1);
+        document.getElementById('main').appendChild(node2);
+        document.getElementById('main').appendChild(node3);
+        document.getElementById('main').appendChild(br);
+
+      });
+
     });
+
   });
 
 });
-
-//var nicej = JSON.stringify(j, null, 2);
-// document.getElementById("main").innerHTML = "<pre>"+ nicej +"</pre>";
-
-// for (var i = 0; i < 10; i++) {
-//   document.getElementById("main").innerHTML = "<pre>"+ j[0]["body"] +"</pre>";
-// };
