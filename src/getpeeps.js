128c57592747bc4e3132cf90d9b1d89e07bbd4e8
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-01 10:33:22 +0000

    can iterate the tweets kind of

--- src/getpeeps.js
+++ src/getpeeps.js
@@ -1,5 +1,24 @@
 fetch('https://chitter-backend-api.herokuapp.com/peeps').then(function(response) {
   return response.json();
-}).then(function(j) {
-  console.log(j);
+}).then(function(data) {
+
+  console.log(data);
+
+  $.each(data, function (index, value) {
+    $.each(this, function (index, value) {
+      //console.log(index + " :: " + value);
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(value);
+      node.appendChild(textnode);
+      document.getElementById("main").appendChild(node);
+    });
+  });
+
 });
+
+//var nicej = JSON.stringify(j, null, 2);
+// document.getElementById("main").innerHTML = "<pre>"+ nicej +"</pre>";
+
+// for (var i = 0; i < 10; i++) {
+//   document.getElementById("main").innerHTML = "<pre>"+ j[0]["body"] +"</pre>";
+// };
