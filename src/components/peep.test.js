1529643b6250219930abcd5f18820598bd74c5b5
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-01 10:57:00 +0100

    Amend Peep test to use toBeInTheDocument

--- src/components/peep.test.js
+++ src/components/peep.test.js
@@ -4,5 +4,5 @@ import Peep from './peep';
 
 test('renders "This is a peep"', () => {
   render(<Peep />);
-  expect(screen.getByText('This is a peep'));
+  expect(screen.getByText('This is a peep')).toBeInTheDocument();
 });
