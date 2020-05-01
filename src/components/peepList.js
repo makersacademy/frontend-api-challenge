93f78263c5eb325de638e0a21e203456723b020f
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-01 12:54:17 +0100

    Add useEffect hook to PeepList to fetch peeps from server

--- src/components/peepList.js
+++ src/components/peepList.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Peep from './peep';
 
 function PeepList() {
+  const [test, setTest] = useState([]);
+
   const peeps = ['This is a peep', 'This is also a peep'].map((peep) => (
     <Peep key={peep} text={peep} />
   ));
+
+  useEffect(() => {
+    async function fetchPeeps() {
+      const peeps2 = await fetch('https://chitter-backend-api-v2.herokuapp.com/peeps');
+      await peeps2.json().then((peeps3) => setTest(peeps3));
+    }
+
+    fetchPeeps();
+  }, []);
+
   return (
     <div>
       {peeps}
+      {JSON.stringify(test)}
     </div>
   );
 }
