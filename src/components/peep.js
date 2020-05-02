91c4dcdeeeb85c787982f3287ec1561a73941dbf
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-02 08:49:01 +0100

    Add basic styling to peep

--- src/components/peep.js
+++ src/components/peep.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Peep({text, userHandle}) {
   return (
-    <div>
-      <div>
+    <div style={{ border: '1px solid black', margin: '5px', padding: '5px' }}>
+      <div style={{ textAlign: 'right' }}>
         {userHandle}
       </div>
       <div>
