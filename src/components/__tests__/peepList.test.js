237f605b09af6dfcb825abcead6f2256b6fd5fda
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-02 07:51:26 +0100

    Amend the PeepList test to mock the fetch call to the api
    This was a massive pain to get working. The eventual steps I took
    were as follows:
    - upgrading testing-library/react to the newest version
    - adding the jest-fetch-mock npm package
    - adding a mutation observer shim, which was added to the global
    window in setupTests.js
    - converting the test to be propery asynchronous
    
    For information on a couple of these steps see here:
    https://github.com/testing-library
    	/dom-testing-library/releases/tag/v7.0.0
    
    For some reason I couldn't get the prefered option on that link to
    work.

--- src/components/__tests__/peepList.test.js
+++ src/components/__tests__/peepList.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import PeepList from '../peepList';
 
-test('renders the two peeps in the list', () => {
+// the anonmyous function has to be async so we can use await
+test('renders the two peeps in the list', async () => {
+  // mocks the next call to fetch, returning the argument specified
+  fetch.mockResponseOnce(JSON.stringify([{ id: '1', body: 'This is a peep' }, { id: 2, body: 'This is also a peep' }]));
+
   render(<PeepList />);
-  expect(screen.getByText('This is a peep')).toBeInTheDocument();
+
+  // await waitFor is needed because component being tested has an asynchronous call using fetch
+  // without this the test will complete before the fetch call does, which fails the test
+  await waitFor(() => expect(screen.getByText('This is a peep')).toBeInTheDocument());
   expect(screen.getByText('This is also a peep')).toBeInTheDocument();
 });
--- src/components/peepList.js
+++ src/components/peepList.js
@@ -2,25 +2,20 @@ import React, { useState, useEffect } from 'react';
 import Peep from './peep';
 
 function PeepList() {
-  const [test, setTest] = useState([]);
-
-  const peeps = ['This is a peep', 'This is also a peep'].map((peep) => (
-    <Peep key={peep} text={peep} />
-  ));
+  const [peeps, setPeeps] = useState([]);
 
   useEffect(() => {
     async function fetchPeeps() {
-      const peeps2 = await fetch('https://chitter-backend-api-v2.herokuapp.com/peeps');
-      await peeps2.json().then((peeps3) => setTest(peeps3));
+      const data = await fetch('https://chitter-backend-api-v2.herokuapp.com/peeps');
+      data.json().then((peepsData) => setPeeps(peepsData));
     }
 
     fetchPeeps();
-  }, []);
+  }, []); // empty array as second arg prevents the hook being repeatedly called
 
   return (
     <div>
-      {peeps}
-      {JSON.stringify(test)}
+      {peeps.map((peep) => (<Peep key={peep.id} text={peep.body} />))}
     </div>
   );
 }
