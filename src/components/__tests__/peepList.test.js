fd5ef4157ba8660cf1079d5c1f6b0726161b3972
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-02 07:51:26 +0100

    Amend the PeepList test to mock the fetch call to the api
    This was a massive pain to get working. The eventual steps I took
    were as follows:
    - upgrading testing-library/react to the newest version
    - adding the jest-fetch-mock npm package
    - adding a mutation observer shim, which was added to the global
    window in setupTests.js
    - converting the test to be propery asynchronous
    
    For information on a couple of these steps see here:
    https://github.com/testing-library
    	/dom-testing-library/releases/tag/v7.0.0
    
    For some reason I couldn't get the prefered option on that link to
    work.

--- src/components/__tests__/peepList.test.js
+++ src/components/__tests__/peepList.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import PeepList from '../peepList';
 
-test('renders the two peeps in the list', () => {
+test('renders the two peeps in the list', async () => {
+  fetch.mockResponseOnce(JSON.stringify([{ id: '1', body: 'This is a peep' }, { id: 2, body: 'This is also a peep' }]));
+
   render(<PeepList />);
-  expect(screen.getByText('This is a peep')).toBeInTheDocument();
+  await waitFor(() => expect(screen.getByText('This is a peep')).toBeInTheDocument());
   expect(screen.getByText('This is also a peep')).toBeInTheDocument();
 });
--- src/components/peepList.js
+++ src/components/peepList.js
@@ -4,14 +4,10 @@ import Peep from './peep';
 function PeepList() {
   const [test, setTest] = useState([]);
 
-  const peeps = ['This is a peep', 'This is also a peep'].map((peep) => (
-    <Peep key={peep} text={peep} />
-  ));
-
   useEffect(() => {
     async function fetchPeeps() {
-      const peeps2 = await fetch('https://chitter-backend-api-v2.herokuapp.com/peeps');
-      await peeps2.json().then((peeps3) => setTest(peeps3));
+      const data = await fetch('https://chitter-backend-api-v2.herokuapp.com/peeps');
+      data.json().then((peeps) => setTest(peeps));
     }
 
     fetchPeeps();
@@ -19,8 +15,7 @@ function PeepList() {
 
   return (
     <div>
-      {peeps}
-      {JSON.stringify(test)}
+      {test.map((peep) => (<Peep key={peep.id} text={peep.body} />))}
     </div>
   );
 }
