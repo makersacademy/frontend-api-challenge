58c62955b3c2804a9458dfab9614e02ae5009323
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-23 16:08:40 +0100

    Add welcome message to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { LogInContext } from './logInContext';
 
 function Header() {
+  const [state, , isLoggedIn] = useContext(LogInContext);
+
   const styles = {
     display: 'flex',
     justifyContent: 'space-between',
@@ -15,7 +18,10 @@ function Header() {
   return (
     <div style={styles}>
       <div>Chitter</div>
-      <div style={{ fontSize: '16px' }}><Link to="/sign-up-log-in">Sign up/Log in</Link></div>
+      <div style={{ fontSize: '16px' }}>
+        <Link to="/sign-up-log-in">Sign up/Log in</Link>
+      </div>
+      {isLoggedIn() && <h2>Welcome {state.handle}</h2>}
     </div>
   );
 }
