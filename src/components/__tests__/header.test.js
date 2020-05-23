e37465e5f51d1a2ba85634b5816ff2f549967839
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-23 16:05:37 +0100

    Add test for header showing welcome message on login
    At present, this test does not work. I cannot work out how to
    mock/use/whatever the context with custom values without having
    to manipulate the functions within the context directly. Given
    this is supposed to be a unit test, I really don't want to have to
    do that.
    
    At the moment, I'm leaning towards it being an issue with the way
    I've implemented the context itself, but I can't see another way
    of doing it while still allowing me to pass functions as part of
    the context state.
    
    I will continue investigating, but for the moment the offending
    test is disabled.

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -2,10 +2,35 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../header';
+import { LogInProvider } from '../logInContext';
 
 test('renders the heading', () => {
   // you need to wrap the component in a router because it contains
   // a Link component. Without being wrapped, the test won't run
-  render(<Router><Header /></Router>);
+  render(
+    <Router>
+      <Header />
+    </Router>
+  );
   expect(screen.getByText('Chitter')).toBeInTheDocument();
+});
+
+test.skip('renders the welcome string when youre logged in', () => {
+  const contextValue = [
+    { handle: 'Bob' },
+    'a',
+    () => {
+      return true;
+    },
+  ];
+
+  render(
+    <Router>
+      <LogInProvider>
+        <Header />
+      </LogInProvider>
+    </Router>
+  );
+
+  expect(screen.getByText('Welcome Phil')).toBeInTheDocument();
 });
