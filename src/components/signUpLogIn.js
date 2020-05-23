fd0746b832e50abf425e1f691f68781f3ac8fdc0
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-23 07:53:45 +0100

    Reformat signupLogIn component

--- src/components/signUpLogIn.js
+++ src/components/signUpLogIn.js
@@ -1,33 +1,41 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function SignUpLogIn() {
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const history = useHistory();
 
   const handleSignup = (evt) => {
     evt.preventDefault();
-    const data = { user: { handle: name, password: password } };
+    const data = { user: { handle: name, password } };
 
     function processSignupResponse(response) {
-      if (response.handle !== undefined && response.handle[0] === 'has already been taken') {
-        setMessage('Sorry, that name has already been taken, please choose another');
+      if (
+        response.handle !== undefined &&
+        response.handle[0] === "has already been taken"
+      ) {
+        setMessage(
+          "Sorry, that name has already been taken, please choose another"
+        );
       } else {
-        setMessage('Successfully signed up. Please log in');
+        setMessage("Successfully signed up. Please log in");
       }
     }
 
     async function sendSignupRequest() {
-      fetch('https://chitter-backend-api-v2.herokuapp.com/users', {
-        method: 'POST',
+      fetch("https://chitter-backend-api-v2.herokuapp.com/users", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
-      }).then((response) => response.json())
-        .then((responseJSON) => { processSignupResponse(responseJSON); });
+      })
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          processSignupResponse(responseJSON);
+        });
     }
 
     sendSignupRequest();
@@ -35,29 +43,32 @@ function SignUpLogIn() {
 
   const handleLogin = (evt) => {
     evt.preventDefault();
-    const data = { session: { handle: name, password: password } };
+    const data = { session: { handle: name, password } };
 
     function processLoginResponse(response) {
       if (response.errors !== undefined) {
-        setMessage('Sorry, invalid username or password, please try again');
+        setMessage("Sorry, invalid username or password, please try again");
       } else {
-        setMessage('All good, nothing to see here');
+        setMessage("All good, nothing to see here");
       }
     }
 
     async function sendLoginRequest() {
-      fetch('https://chitter-backend-api-v2.herokuapp.com/sessions', {
-        method: 'POST',
+      fetch("https://chitter-backend-api-v2.herokuapp.com/sessions", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
-      }).then((response) => response.json())
-        .then((responseJSON) => { processLoginResponse(responseJSON); });
+      })
+        .then((response) => response.json())
+        .then((responseJSON) => {
+          processLoginResponse(responseJSON);
+        });
     }
 
     sendLoginRequest();
-    //history.push('/peeps');
+    history.push("/peeps");
   };
 
   return (
@@ -65,19 +76,33 @@ function SignUpLogIn() {
       <form>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div>
-          <button type="button" onClick={handleSignup}>Sign up</button>
-          <button type="button" onClick={handleLogin}>Log in</button>
+          <button type="button" onClick={handleSignup}>
+            Sign up
+          </button>
+          <button type="button" onClick={handleLogin}>
+            Log in
+          </button>
         </div>
-        <div>
-          {message}
-        </div>
+        <div>{message}</div>
       </form>
     </div>
   );
