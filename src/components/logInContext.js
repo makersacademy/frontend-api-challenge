b32a55e361e7d334e55d181e9a3d1d6342a62cb0
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-23 14:40:32 +0100

    Add login context

---
+++ src/components/logInContext.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const LogInContext = React.createContext([{}, () => {}, () => {}]);
+
+function LogInProvider({ children }) {
+  const [state, setState] = useState({ user_id: '', session_key: '' });
+
+  function login(loginState, name) {
+    setState({
+      user_id: loginState.user_id,
+      session_key: loginState.session_key,
+      handle: name,
+    });
+  }
+
+  function isUserLoggedIn() {
+    return state.user_id !== '';
+  }
+
+  return (
+    <LogInContext.Provider value={[state, login, isUserLoggedIn]}>
+      {children}
+    </LogInContext.Provider>
+  );
+}
+
+export { LogInContext, LogInProvider };
