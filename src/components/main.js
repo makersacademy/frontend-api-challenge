61a2de4e0108de8758c36f22851890c91a370dec
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-23 14:41:32 +0100

    Update Main component to provide context

--- src/components/main.js
+++ src/components/main.js
@@ -4,17 +4,20 @@ import { Redirect, Route, useLocation } from 'react-router-dom';
 import Header from './header';
 import PeepList from './peepList';
 import SignUpLogIn from './signUpLogIn';
+import { LogInProvider } from './logInContext';
 
 function Main() {
   const path = useLocation().pathname;
 
   return (
-    <div>
-      { path === '/' ? <Redirect to="/peeps" /> : null }
-      <Header />
-      <Route path="/sign-up-log-in" exact component={SignUpLogIn} />
-      <Route path="/peeps" exact component={PeepList} />
-    </div>
+    <LogInProvider>
+      <div>
+        {path === '/' ? <Redirect to="/peeps" /> : null}
+        <Header />
+        <Route path="/sign-up-log-in" exact component={SignUpLogIn} />
+        <Route path="/peeps" exact component={PeepList} />
+      </div>
+    </LogInProvider>
   );
 }
 
