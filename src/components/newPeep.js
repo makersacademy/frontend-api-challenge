d5d0faab52c0938f383724a0a918e4958b8b3b00
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-25 12:47:54 +0100

    Create simple NewPeep component

---
+++ src/components/newPeep.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NewPeep() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="text">Text:</label>
+        <input type="text" name="text" id="text" />
+      </form>
+    </div>
+  );
+}
+
+export default NewPeep;
