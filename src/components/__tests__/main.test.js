9bd50c8552fe7af39377e7448b56a0df787206ad
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-25 12:57:36 +0100

    Move the login context provider from Main to App
    This will make testing the Main component easier, as it will
    allow the context state to be mocked.

--- src/components/__tests__/main.test.js
+++ src/components/__tests__/main.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Main from '../main';
+import LoginContext from '../loginContext';
 
 beforeAll(() => {
   fetch.mockResponse(JSON.stringify([]));
@@ -10,12 +11,40 @@ beforeAll(() => {
 // wrapping the component in Router ensures that the links in the header within
 // main work properly. Without doing this the test will crash
 test('renders the peeps list when the current path is / or /peeps', async () => {
-  render(<Router><Main /></Router>);
+  render(
+    <Router>
+      <LoginContext.Provider
+        value={[
+          '',
+          null,
+          () => {
+            return true;
+          },
+        ]}
+      >
+        <Main />
+      </LoginContext.Provider>
+    </Router>
+  );
   await waitFor(() => expect(screen.getByText('Peeps')).toBeInTheDocument());
 });
 
 test('renders signup when you click the signup link', async () => {
-  render(<Router><Main /></Router>);
+  render(
+    <Router>
+      <LoginContext.Provider
+        value={[
+          '',
+          null,
+          () => {
+            return false;
+          },
+        ]}
+      >
+        <Main />
+      </LoginContext.Provider>
+    </Router>
+  );
 
   // you have to wait here for the peeps to load, otherwise the test will crash
   await waitFor(() => expect(screen.getByText('Peeps')));
--- src/components/main.js
+++ src/components/main.js
@@ -1,33 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Redirect, Route, useLocation } from 'react-router-dom';
 
 import Header from './header';
 import PeepList from './peepList';
 import SignUpLogIn from './signUpLogIn';
-import LoginContext from './loginContext';
 
 function Main() {
   const path = useLocation().pathname;
-  const initialLoginState = { user_id: '', session_key: '', handle: '' };
-  const [loginState, setLoginState] = useState(initialLoginState);
-
-  function login(userId, sessionKey, handle) {
-    setLoginState({ user_id: userId, session_key: sessionKey, handle });
-  }
-
-  function isLoggedIn() {
-    return loginState.session_key !== '';
-  }
 
   return (
-    <LoginContext.Provider value={[loginState, login, isLoggedIn]}>
-      <div>
-        {path === '/' ? <Redirect to="/peeps" /> : null}
-        <Header />
-        <Route path="/sign-up-log-in" exact component={SignUpLogIn} />
-        <Route path="/peeps" exact component={PeepList} />
-      </div>
-    </LoginContext.Provider>
+    <div>
+      {path === '/' ? <Redirect to="/peeps" /> : null}
+      <Header />
+      <Route path="/sign-up-log-in" exact component={SignUpLogIn} />
+      <Route path="/peeps" exact component={PeepList} />
+    </div>
   );
 }
 
