eea11ba33cb1fa2ddf499038124d3a5047488fa6
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-31 12:19:35 +0100

    Console log test api call

--- src/interface.js
+++ src/interface.js
@@ -3,9 +3,16 @@
 $(document).ready(function() {
 
   updateAppName();
+  testApiCall();
 
   function updateAppName() {
     $(`#appName`).text(`Chitter`);
+  }
+
+  function testApiCall() {
+    fetch(`https://chitter-backend-api-v2.herokuapp.com/peeps`)
+      .then(response => response.json())
+      .then(data => console.log(data));
   }
 
 });
