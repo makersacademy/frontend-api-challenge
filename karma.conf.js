0649c3704c39b6503c12693377e0ba0dafafc42d
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-31 12:33:28 +0100

    Adds Spec and Coverage to Karma output

--- karma.conf.js
+++ karma.conf.js
@@ -30,13 +30,18 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'src/*.js': ['coverage']
     },
 
+    coverageReporter: {
+      type : 'text',
+      dir : 'coverage/'
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec', 'coverage'],
 
 
     // web server port
