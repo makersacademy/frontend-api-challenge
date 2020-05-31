4211e15e572c2a556ec0dc6cf506728c10426970
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-31 15:16:33 +0100

    made peeps a link

--- interface.js
+++ interface.js
@@ -4,15 +4,17 @@ let header = document.createElement('h1')
 header.innerHTML = 'Chitter'
 
 fetch('https://chitter-backend-api-v2.herokuapp.com/peeps.json')
-.then(response => response.json())
-.then(function(data) {
+  .then(response => response.json())
+  .then(function(data) {
   document.body.appendChild(header)
   let chitter = data
   return chitter.map(function(peep) {
     let user = document.createElement('p')
-    let list = document.createElement('p')
+    let list = document.createElement('a')
     let br = document.createElement('br')
     user.innerHTML = `${peep.user["handle"]}`
+    list.setAttribute('href', '/')
+    list.setAttribute('class', 'peep')
     list.innerHTML = `${peep.body}`
     document.body.appendChild(user)
     document.body.appendChild(list)
