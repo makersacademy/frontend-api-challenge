0d0e88e86d1aa528ca208d9e52872a0c5d277570
Rachael <rachaelewins@hotmail.com>
2020-06-28 11:47:48 +0100

    using the single piepja API connection

--- src/singlePiepje.js
+++ src/singlePiepje.js
@@ -1,8 +1,15 @@
-function peipjeNumber(number) {
-  requestPeipjesList().then(function(data) {
-    var peipBody = `<div id="body">Peipje: ${data[number].body}</div>`
-    var peipUser = `<div id="user">User: ${data[number].user.handle}</div>`
-    var peipTime = `<div id="time">Time: ${data[number].created_at}</div>`
+async function requestPeipje(id) {
+  await fetch(`https://chitter-backend-api-v2.herokuapp.com/peeps/${id}`)
+  .then(response => out = response.json())
+  console.log(out)
+  return out
+};
+
+function peipjeID(id) {
+  requestPeipje(id).then(function(data) {
+    var peipBody = `<div id="body">Peipje: ${data.body}</div>`
+    var peipUser = `<div id="user">User: ${data.user.handle}</div>`
+    var peipTime = `<div id="time">Time: ${data.created_at}</div>`
     var peipjeData = peipUser + peipTime + peipBody
     document.getElementById('mainDisplay').innerHTML = peipjeData
   })
