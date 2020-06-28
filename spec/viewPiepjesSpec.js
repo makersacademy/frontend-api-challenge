2ffdba46772afb60e5dbfb2accc4cc9d0a597e98
Rachael <rachaelewins@hotmail.com>
2020-06-28 10:50:50 +0100

    shows peip and how created it

--- spec/viewPiepjesSpec.js
+++ spec/viewPiepjesSpec.js
@@ -1,8 +1,9 @@
 function testCanGetPeipjeDataFromAPI() {
   peipjesList()
   setTimeout(() => {
-    console.log(document.getElementById('peipjesList').innerHTML)
-    expect(document.getElementById('peipjesList').innerHTML === "qwer", "reading data from API")
-  }, 200);
+    console.log(document.getElementById('peip0/body').innerHTML)
+    expect(document.getElementById('peip0/body').innerHTML === "qwer", "reading data from API body")
+    expect(document.getElementById('peip0/user').innerHTML === "qwer", "reading data from API user")
+  }, 500);
 }
 testCanGetPeipjeDataFromAPI()
--- src/viewPiepjes.js
+++ src/viewPiepjes.js
@@ -7,8 +7,10 @@ async function requestPeipjesList() {
 function peipjesList() {
 
   requestPeipjesList().then(function(data) {
-    console.log(data)
-    document.getElementById('peipjesList').innerHTML = data[0].body
+    console.log(data[0])
+    var peip0body = `<div id="peip0/body">${data[0].body}</div>`
+    var peip0user = `<div id="peip0/user">${data[0].user.handle}</div>`
+    document.getElementById('peipjesList').innerHTML = peip0body + peip0user
   })
 
 }
