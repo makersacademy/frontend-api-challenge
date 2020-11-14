b1e58814820494b49887e625ef6a2b78389a1db9
charlie-galb <galbraith.charlie@gmail.com>
2020-11-14 14:12:42 +0000

    Features: sign in/out buttons react to session data, appearing and disappearing accordingly

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
-  updatePeepList(exportPeepListAsHTML);
   const currentUser = new CurrentUser();
+  updateButtonDisplay();
+  updatePeepList(exportPeepListAsHTML);
 
   $('#sign-up').click(function() {
     event.preventDefault();
@@ -12,15 +13,34 @@ $(document).ready(function() {
     event.preventDefault();
     let newUserHandle = $('#sign-up-handle').val();
     let newUserPassword = $('#sign-up-password').val();
-    console.log($('#sign-up-handle').val());
     sendNewUserData(newUserHandle, newUserPassword);
     sendNewSessionData(newUserHandle, newUserPassword);
+    $('#sign-up-form').addClass('invisible');
+    // $('#form-space').text(`Welcome, ${currentUser.handle}`)
   });
 
   $('#log-in').click(function() {
     event.preventDefault();
     $('#log-in-form').removeClass('invisible');
   });
+
+  $('#log-in-form').submit(function(event) {
+    event.preventDefault();
+    let userHandle = $('#log-in-handle').val();
+    let userPassword = $('#log-in-password').val();
+    sendNewSessionData(userHandle, userPassword);
+    $('#log-in-form').addClass('invisible');
+    updatePeepList(exportPeepListAsHTML);
+  });
+
+  $('#sign-out').click(function(event) {
+    event.preventDefault();
+    $('#user-messages').text('');
+    currentUser._handle = null;
+    currentUser._id = null;
+    currentUser._sessionKey = null;
+    updateButtonDisplay();
+  })
 
   function updatePeepList() {
     $.get("https://chitter-backend-api-v2.herokuapp.com/peeps", function(data) {
@@ -37,9 +57,6 @@ $(document).ready(function() {
       dataType: 'json',
       contentType: 'application/json',
       success: function(data){
-        console.log(data);
-        currentUser._handle = data.handle;
-        console.log(currentUser.handle)
         sendNewSessionData(handle, password)
       },
       error: function(e){ console.log(e)}
@@ -54,14 +71,31 @@ $(document).ready(function() {
       dataType: 'json',
       contentType: 'application/json',
       success: function(data){
+        currentUser._handle = handle;
         currentUser._id = data.user_id;
         currentUser._sessionKey = data.session_key;
-        console.log(currentUser);
-
+        updateButtonDisplay()
+        newSessionGreeter();
       },
       error: function(e){ console.log(e)}
       });
   }
+
+  function newSessionGreeter() {
+    $('#user-messages').text(`Welcome, ${currentUser.handle}`);
+  }
+
+  function updateButtonDisplay() {
+    if (currentUser.sessionKey) {
+      $('#sign-up').addClass('invisible');
+      $('#log-in').addClass('invisible');
+      $('#sign-out').removeClass('invisible');
+    } else {
+      $('#sign-up').removeClass('invisible');
+      $('#log-in').removeClass('invisible');
+      $('#sign-out').addClass('invisible');
+      }
+    }
 })
 
   // function postNewPeep() {
