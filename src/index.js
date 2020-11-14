c0801880b38a5058e585ae8dee104d37f8af7c36
charlie-galb <galbraith.charlie@gmail.com>
2020-11-14 11:57:51 +0000

    Feature: Add #sign-up-form that is invisible until the user clicks sign-up

--- src/index.js
+++ src/index.js
@@ -2,11 +2,57 @@ $(document).ready(function() {
 
   updatePeepList(exportPeepListAsHTML);
 
+  $('#sign-up').click(function() {
+    event.preventDefault();
+    $('#sign-up-form').removeClass('invisible');
+  });
+
+  $('#sign-up-form').submit(function(event) {
+    event.preventDefault();
+    let newUserHandle = $('#sign-up-handle').val();
+    let newUserPassword = $('#sign-up-password').val();
+    sendNewUserData(newUserHandle, newUserPassword);
+  });
+
+  $('#log-in').click(function() {
+    event.preventDefault();
+    $('#log-in-form').removeClass('invisible');
+  });
+
   function updatePeepList() {
     $.get("https://chitter-backend-api-v2.herokuapp.com/peeps", function(data) {
       let peepListHTML = exportPeepListAsHTML(data);
       $('#peep-list').html(peepListHTML);
-  });
-};
+    });
+  };
 
+  function sendNewUserData(handle, password) {
+    $.ajax({
+      url: 'https://chitter-backend-api-v2.herokuapp.com/users',
+      type: 'POST',
+      data: JSON.stringify({user: {handle: `${handle}`, password:`${password}`}}),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: function(data){
+        console.log('New user created' + data)
+      },
+      error: function(e){ console.log(e)}
+      });
+  }
 })
+
+  // function postNewPeep() {
+  //   $.ajax({
+  //   url: 'https://chitter-backend-api-v2.herokuapp.com/peeps',
+  //   type: ‘POST’,
+  //   headers: {“Authorization”: 'Token _2a_12_TS6ot62h5KbjB_Z_3xmP6e'},
+  //   data: JSON.stringify({peep: {user_id:'135', body:'my first peep'}}),
+  //   dataType: 'json',
+  //   contentType: 'application/json',
+  //   success: function(data){
+  //     console.log('HI')
+  //     console.log(data)
+  //   },
+  //   error: function(e){ console.log(e)}
+  //   });
+  //   }
--- src/peep-list.js
+++ src/peep-list.js
@@ -2,6 +2,5 @@
 
 function exportPeepListAsHTML(peeps){
   let list = peeps.map(peep => `<li><div id='${peep.id}'><p>${peep.body}</p><br><i>${peep.user.handle}</i></div></li>`).join("");
-  console.log(list)
   return "<ul>" + list + "</ul>"
 }
