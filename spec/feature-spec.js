b572f4c34d810e680bbc75892ee2046b564789db
charlie-galb <galbraith.charlie@gmail.com>
2020-11-14 03:23:42 +0000

    Debugging: feature test now passes after adding setTimeout

--- spec/feature-spec.js
+++ spec/feature-spec.js
@@ -2,10 +2,13 @@
 
 describe('index.html', function(){
 
-  var peep-list = document.getElementById('#peep-list');
+  var peepList = document.getElementById('#peep-list');
 
   it('shows a list of peeps in chronological order', function(){
-    expect(peep-list.innerHTML).toEqual("<ul><li><div id='3'><p>my first peep :)</p><br><i>kay</i></div></li></ul>")
+    setTimeout(function(){
+      expect(peepList.innerHTML).toEqual("<ul><li><div id='3'><p>my first peep :)</p><br><i>kay</i></div></li></ul>");
+    }, 1000);
+
   })
 
 
