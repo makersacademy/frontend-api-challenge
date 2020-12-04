5dfb15811899992a6077cdc6f385cbc18ee88dcf
AnastasisGi <anastasiosgks@gmail.com>
2020-12-04 12:57:08 +0000

    Can show fields of login and sing up & field can disapear

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,26 +1,40 @@
 const output = document.querySelector('.peeps-bucket')
+const signUpBtn = document.getElementById('sign-up');
+const logInForm = document.getElementById('log-in-form');
 
 window.addEventListener('load', ()=>{
 
+// Call the function get the peeps and call them .
+  updatePeepsList();
+// Get the peeps and post them
+  function updatePeepsList(){
+    const url = 'https://chitter-backend-api-v2.herokuapp.com/peeps'
+    fetch(url).then(function(res){
+      return res.json()
+    }).then(function(data){
+      data.forEach(function(peeps){
+        output.innerHTML += `${peeps.body} <br>posted by ${peeps.user.handle} <br><br>`
+      })
+    })
+  }
 
-updatePeepsList();
+
+  let callOfThesignUpBtn = function(){
+    console.log(1,'SignUp');
+    logInForm.hidden = true;
+  }
+//Add event listeners - event Listener for the signup btn
+signUpBtn.addEventListener('click',callOfThesignUpBtn)
+
+// Clicks the signup button
+
+
+// Sends the data for the username
 
 
 
 
-function updatePeepsList(){
-console.log("1");
-const url = 'https://chitter-backend-api-v2.herokuapp.com/peeps'
-fetch(url).then(function(res){
-  return res.json()
-}).then(function(data){
-  console.log(data);
-  data.forEach(function(peeps){
-    console.log(peeps.body);
-    output.innerHTML += `${peeps.body} <br>posted by ${peeps.user.handle} <br><br>`
-  })
-})
-}
+// Sends the data for the session
 
 
 
