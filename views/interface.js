501b31ba18c3f3b1aa3c1a442f35734b19bc890d
Chris Abdo <christophermabdo@icloud.com>
2021-01-24 17:53:09 +0000

    added viewing a singular Peep function

--- views/interface.js
+++ views/interface.js
@@ -30,6 +30,48 @@ function getPostData() {
  })
  
 }
+let button = document.getElementById('find-peep')
+button.addEventListener("click", function() {
+    let area = document.getElementById('peep-id')
+    let id = area.value
+    area.value = ""
+    getSinglePeep(id)
+    // emojifyText()
+    })
+
+function getSinglePeep(id) {
+    
+    fetch("https://chitter-backend-api-v2.herokuapp.com/peeps" + `/${id}`).then(response => {
+    response.json().then(post => {
+    console.log(post)
+     document.getElementById('peeps').innerHTML = `${post.body}`;
+    //  post.forEach((element, index, array) => {
+    //     // var node = document.createElement("LI"); 
+    //      let peeps = element.body
+
+    //      document.getElementById("peeps").append(peeps)
+    //      document.getElementById("peeps").innerHTML += "<br>";
+         
+    //     })
+        // let rendered = renderPost(element);
+        // document.getElementById("peeps").innerHTML = rendered;
+    // var keys = []
+    // var values = []
+    // for (var i = 0; i < post.length; i++) {
+    //     for (var key in post[i]) {
+    //         if (post[i].hasOwnProperty(key)) {
+    //             keys.push(key);
+    //             values.push(post[i][key]);
+    //         }
+    //     }
+    // }
+    // // document.getElementById("peeps").innerHTML = keys;
+    // document.getElementById("peeps").innerHTML = values;
+   })
+   
+ })
+ 
+}
 
 function renderPost(postData) {
     let postHeadingHTML = `<p>${postData.id}</p>`;
