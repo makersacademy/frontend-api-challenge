8a2b3dde29105c96c88157e68965b2f10169e754
Aniruddh <ojhaa94@gmail.com>
2021-03-21 16:53:34 +0000

    adds formatting to peep feed

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ function viewAllPeeps() {
 		return response.json();
 	// # Data
 	}).then(data => {
+		// OPTION 1
 		let html = ""
 		for (let i = 0; i < 25; i ++) {
-			html += `<p>${data[i].user.handle} peeped: ${data[i].body}</p>`
+			html += `<div id="peep">
+									<p><b>@${data[i].user.handle}</b> peeped:</p>
+									<p id="body">${data[i].body}</p>
+									<p id="time">at ${data[i].created_at}</p>
+							 </div>`
+
 		}
 		feed.innerHTML = html;
+
+
 		// data.forEach(peep => {
 		// 	console.log(peep.body)
 		// 	console.log(peep.user.handle)
