faf44d1fd8ddd0675273fa08dd1586bfc9155611
Jessica Brand <jeshbrand@gmail.com>
2021-05-15 18:24:18 +0100

    minor formatting improvement

--- src/interface.js
+++ src/interface.js
@@ -8,11 +8,11 @@ users = 'https://chitter-backend-api-v2.herokuapp.com/users'
 peeps = 'https://chitter-backend-api-v2.herokuapp.com/peeps'
 
 // fetchMe(users).then(results => console.log(results))
-// fetchMe(peeps).then(results => console.log(results))
+fetchMe(peeps).then(results => console.log(results))
 
 var allPeeps = document.querySelector('#allPeeps')
 fetchMe(peeps).then((result) => {
   result.forEach(peep =>
     // allPeeps.innerHTML += peep.body + "<br />");
-    allPeeps.innerHTML += `<a href="PLACEHOLDER">${peep.body}</a> <br>`);
+    allPeeps.innerHTML += `<a href="PLACEHOLDER">${peep.body}</a> - <small> said ${peep.user.handle}</small> <br>`);
 })
