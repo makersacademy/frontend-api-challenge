d69eb06844c06a81310ae4cdaf9fded6debf0d34
Jessica Brand <jeshbrand@gmail.com>
2021-05-15 19:21:50 +0100

    can now go to individual peep pages

--- src/interface.js
+++ src/interface.js
@@ -4,15 +4,28 @@ async function fetchMe(url){
   return response.json()
 }
 
-users = 'https://chitter-backend-api-v2.herokuapp.com/users'
-peeps = 'https://chitter-backend-api-v2.herokuapp.com/peeps'
+const users = 'https://chitter-backend-api-v2.herokuapp.com/users'
+const peeps = 'https://chitter-backend-api-v2.herokuapp.com/peeps'
 
 // fetchMe(users).then(results => console.log(results))
-fetchMe(peeps).then(results => console.log(results))
+// fetchMe(peeps).then(results => console.log(results))
 
-var allPeeps = document.querySelector('#allPeeps')
+const allPeeps = document.querySelector('#allPeeps')
+const returnHome = document.querySelector('#backHome')
+
+// homepage
 fetchMe(peeps).then((result) => {
   result.forEach(peep =>
     // allPeeps.innerHTML += peep.body + "<br />");
-    allPeeps.innerHTML += `<a href="PLACEHOLDER">${peep.body}</a> - <small> said ${peep.user.handle}</small> <br>`);
+    allPeeps.innerHTML += `<a href="#${peep.id}">${peep.body}</a> - <small> said ${peep.user.handle}</small> <br>`
+  );
+})
+
+// for seeing individual peeps
+window.addEventListener('hashchange',()=>{
+  allPeeps.innerHTML = '';
+  returnHome.innerHTML = `<a href="">back to main page</a>`;
+  console.log(location.hash.slice(1))
+  fetchMe(`https://chitter-backend-api-v2.herokuapp.com/peeps/${location.hash.slice(1)}`)
+    .then(result => allPeeps.innerHTML += `${result.body}`)
 })
