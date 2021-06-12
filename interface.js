46ad2a95898dd4b53ecb82e1204f25be404ab456
BecaLParker <becaparker@gmail.com>
2021-06-12 13:21:55 +0100

    Displays quacks on pageload

--- interface.js
+++ interface.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const quackboard = new QuackBoard()
 
-  quackboard.getQuacks().then((data) => {
-    document.querySelector('#current-quacks').innerText = data
+  quackboard.getQuacks().then((quacks) => {
+    let ul = document.getElementById("current-quacks");
+    ul.innerHTML = "";
+    quacks.forEach((quack) => {
+      let li = document.createElement("li");
+      li.innerHTML = `"${quack.body}" quacked by ${quack.user.handle}, at ${quack.created_at}`
+      ul.appendChild(li);
+    })
+    
   })
 })
