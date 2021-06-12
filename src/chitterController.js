9dbcfb1c73acd0d643078e51725d0824c048e6cc
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-12 15:42:45 +0100

    getPeep fetches peep and passes to view

--- src/chitterController.js
+++ src/chitterController.js
@@ -8,6 +8,12 @@ class ChitterController {
       .then(response => response.json())
       .then(peeps => this._view.displayPeeps(peeps))
   }
+
+  async getPeep (peepId) {
+    await fetch(`https://chitter-backend-api-v2.herokuapp.com/peeps/${peepId}`)
+      .then(response => response.json())
+      .then(peep => this._view.displayPeep(peep))
+  }
 }
 
 module.exports = ChitterController
--- src/chitterController.test.js
+++ src/chitterController.test.js
@@ -28,7 +28,8 @@ describe('ChitterController', () => {
   let chitterController
 
   const viewDouble = {
-    displayPeeps: () => {}
+    displayPeeps: () => {},
+    displayPeep: () => {}
   }
   const spyDisplayPeepsFn = jest.spyOn(viewDouble, 'displayPeeps')
 
@@ -64,6 +65,89 @@ describe('ChitterController', () => {
 
         expect(fetch).toHaveBeenCalledWith('https://chitter-backend-api-v2.herokuapp.com/peeps')
         expect(spyDisplayPeepsFn).toHaveBeenCalledWith(examplePeepsArray2)
+      })
+    })
+  })
+
+  const examplePeep1 = {
+    id: 1,
+    body: 'Congrats for successfully requesting the peeps from the Chitter API! This is the first peep :)',
+    created_at: '2020-03-06T17:16:22.601Z',
+    updated_at: '2020-03-06T17:16:22.601Z',
+    user: {
+      id: 1,
+      handle: 'edward'
+    },
+    likes: [
+      {
+        user: {
+          id: 162,
+          handle: 'Kristof'
+        }
+      },
+      {
+        user: {
+          id: 170,
+          handle: 'amit'
+        }
+      },
+      {
+        user: {
+          id: 40,
+          handle: 'maria'
+        }
+      },
+      {
+        user: {
+          id: 92,
+          handle: 'Mokk'
+        }
+      }
+    ]
+  }
+
+  const examplePeep2 = {
+    id: 485,
+    body: 'still now?',
+    created_at: '2021-06-02T09:47:00.147Z',
+    updated_at: '2021-06-02T09:47:00.147Z',
+    user: {
+      id: 454,
+      handle: 'wishing2'
+    },
+    likes: []
+  }
+
+  const spyDisplayPeepFn = jest.spyOn(viewDouble, 'displayPeep')
+
+  describe('.prototype.getPeep()', () => {
+    describe('when fetch() resolves to examplePeep1', () => {
+      test('fetches data and tells the view to display Peep', async () => {
+        global.fetch = jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve(examplePeep1)
+          })
+        )
+
+        await chitterController.getPeep(1)
+
+        expect(fetch).toHaveBeenCalledWith('https://chitter-backend-api-v2.herokuapp.com/peeps/1')
+        expect(spyDisplayPeepFn).toHaveBeenCalledWith(examplePeep1)
+      })
+    })
+
+    describe('when fetch() resolves to examplePeep2', () => {
+      test('fetches data and tells the view to display Peep', async () => {
+        global.fetch = jest.fn(() =>
+          Promise.resolve({
+            json: () => Promise.resolve(examplePeep2)
+          })
+        )
+
+        await chitterController.getPeep(485)
+
+        expect(fetch).toHaveBeenCalledWith('https://chitter-backend-api-v2.herokuapp.com/peeps/485')
+        expect(spyDisplayPeepFn).toHaveBeenCalledWith(examplePeep2)
       })
     })
   })
