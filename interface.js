1059a9801494be71c900bc2e4b8876036c7d56ac
Matt Firmston <mattfirmston@gmail.com>
2021-08-15 14:02:36 +0100

    displays peeps on homepage

--- interface.js
+++ interface.js
@@ -1,3 +1,18 @@
-document.addEventListener(DOMContentLoaded, () => {
+document.addEventListener("DOMContentLoaded", () => {
+  displayPeeps();
   
+  function displayPeeps() {
+    fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
+      .then(response => response.json())
+      .then(data => data.forEach(createPeepDiv));
+  }
+
+  function createPeepDiv(peep) {
+    const div = document.createElement('div');
+    div.className = 'peep-div';
+    div.id = peep.id
+    div.innerHTML = `<p class="peep-name">${peep.user.handle}</p>
+    <p class="peep-body">${peep.body}</p>`;
+    document.querySelector('#peeps').appendChild(div);
+  }
 })
