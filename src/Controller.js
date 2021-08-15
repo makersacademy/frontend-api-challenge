03333b86a2cb8432cca5cc17d364166e660c5cc1
lushcodes <lushcodes@gmail.com>
2021-08-15 13:37:47 +0100

    Refactor

--- src/Controller.js
+++ src/Controller.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => { 
 
   const userForm = document.querySelector('#user-details');
+  const signUp = document.querySelector('#sign-up');
+  const logIn = document.querySelector('#log-in');
 
-  userForm.addEventListener('submit', (e) => {
+  signUp.addEventListener('click', (e) => {
     e.preventDefault();
-    postUserData();
+    const formData = new FormData(userForm);
+    const userData = formatUserCredentials(formData);
+    postUserData(userData);
+    getSession(userData);
   });
+
+  logIn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const formData = new FormData(userForm);
+    const userData = formatUserCredentials(formData);
+    getSession(userData);
+  });
+  
 
 getPeeps = () => {
   fetch("https://chitter-backend-api-v2.herokuapp.com/peeps")
@@ -27,24 +40,33 @@ populateDiv = (peep) => {
   document.querySelector('#peeps').appendChild(div);
 }
 
-getUserData = () => {
-  console.log('form submitted');
-  const formData = new FormData(userForm);
+  formatUserCredentials = (formData) => {
   const handle = formData.get('handle');
-  // console.log(handle);
   const password = formData.get('password');
-  // console.log(password);
-  const userData = { "handle":handle, "password":password
-  };
-  // console.log(userData)
+  const userData = { "handle":handle, "password":password };
   return userData
 }
 
-postUserData = () => {
+postUserData = (userData) => {
   fetch("https://chitter-backend-api-v2.herokuapp.com/users", {
     method: "POST",
     headers: { 'Content-Type' : 'application/json' },
-    body: JSON.stringify({"user": getUserData() })
+    body: JSON.stringify({"user": userData })
+    })
+  .then(response => {
+    console.log(response);
+    return response.json()
+  })
+  .then(json => {
+    console.log(json)
+  })
+}
+
+getSession = (userData) => {
+  fetch("https://chitter-backend-api-v2.herokuapp.com/sessions", {
+    method: "POST",
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify({"session": userData })
     })
   .then(response => {
     console.log(response);
