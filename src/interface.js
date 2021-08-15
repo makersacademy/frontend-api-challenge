8649fc761ed5f3ba77efa6540669289452fb3e42
Pav Rao <pav0107@gmail.com>
2021-08-15 10:52:17 +0100

    create a new session giving a user_id and session_key required to perform actions on behalf of the user (e.g. posting peeps, liking peeps).

--- src/interface.js
+++ src/interface.js
@@ -1,17 +1,37 @@
 const signUp = document.getElementById('signUp');
-const handle = document.getElementById('handle');
-const password = document.getElementById('password');
+const logIn = document.getElementById('logIn');
+const signUpHandle = document.getElementById('signUpHandle');
+const signUpPassword = document.getElementById('signUpPassword');
+const logInHandle = document.getElementById('logInHandle');
+const logInPassword = document.getElementById('logInPassword');
 
 signUp.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('hello');
-  console.log(handle.value, password.value);
 
   fetch('https://chitter-backend-api-v2.herokuapp.com/users', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
-      user: { handle: handle.value, password: password.value },
+      user: { handle: signUpHandle.value, password: signUpPassword.value },
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+});
+
+logIn.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(logInHandle.value, logInPassword.value);
+
+  fetch('https://chitter-backend-api-v2.herokuapp.com/sessions', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      session: { handle: logInHandle.value, password: logInPassword.value },
     }),
   })
     .then((response) => {
