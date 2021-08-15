f374dfe75528b590a47e5ed6dd741597d0096cb3
Matt Firmston <mattfirmston@gmail.com>
2021-08-15 15:29:41 +0100

    displays time with peep

--- interface.js
+++ interface.js
@@ -8,11 +8,26 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function createPeepDiv(peep) {
+    const link = document.createElement('a')
+    link.href = `#${peep.id}`
     const div = document.createElement('div');
     div.className = 'peep-div';
-    div.id = peep.id
+    const peepTime = formatTime(new Date(peep.updated_at))
     div.innerHTML = `<p class="peep-name">${peep.user.handle}</p>
-    <p class="peep-body">${peep.body}</p>`;
-    document.querySelector('#peeps').appendChild(div);
+    <p class="peep-body">${peep.body}</p>
+    <p class="peep-time">${peepTime}</p>`;
+    link.appendChild(div);
+    document.querySelector('#peeps').appendChild(link);
+  }
+
+  function formatTime(time) {
+    const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat","Sun"]
+    const hours = time.getHours()
+    const minutes = ("0" + time.getMinutes()).slice(-2)
+    const day = days[time.getDay()]
+    const date = time.getDate()
+    const month = time.getMonth() + 1
+    const year = time.getYear() % 100
+    return `${hours}:${minutes} ${day} ${date}/${month}/${year}`
   }
 })
