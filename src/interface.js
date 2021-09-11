8df2a27dd3c607fccce3ce565b2be92c80450c0c
Radu Necsanu <necsanu_radu@yahoo.com>
2021-09-11 22:57:46 +0100

    first commit interface done

---
+++ src/interface.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function registerTooltips() {
+    let tooltipTriggerList = [].slice.call(
+      document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    );
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+  }
+  registerTooltips();
+
+  function registerLoginModal() {
+    let loginModal = new bootstrap.Modal(document.querySelector("#loginModal"));
+    document.querySelector("#callLogin").addEventListener("click", function () {
+      loginModal.show();
+    });
+  }
+  registerLoginModal();
+
+  function registerRegisterModal() {
+    let registerModal = new bootstrap.Modal(
+      document.querySelector("#registerModal")
+    );
+    document.querySelector("#callRegis").addEventListener("click", function () {
+      registerModal.show();
+    });
+  }
+  registerRegisterModal();
+
+  function registerPeepModal() {
+    let peepModal = new bootstrap.Modal(document.querySelector("#peepModal"));
+    document.querySelector("#callPeep").addEventListener("click", function () {
+      peepModal.show();
+    });
+  }
+  registerPeepModal();
+
+  function registerSubmitLogin() {
+    let submitLogin = document.querySelector("#loginModalSubmit");
+    submitLogin.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(submitLogin.parentElement.textContent);
+      return;
+    });
+  }
+  registerSubmitLogin();
+
+  function registerSubmitRegis() {
+    let submitRegister = document.querySelector("#regisModalSubmit");
+    submitRegister.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(submitRegister.parentElement.textContent);
+      return;
+    });
+  }
+  registerSubmitRegis();
+
+  function registerSubmitPeep() {
+    let submitPeep = document.querySelector("#peepModalSubmit");
+    submitPeep.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log(submitPeep.parentElement.textContent);
+      return;
+    });
+  }
+  registerSubmitPeep();
+});
+
+/*
+<i class="fa fa-user mr-2"> &nbsp;</i>
+*/
