f4f5125c950834385e2e8aca58ca8ef64098ca25
Corinne <corinne.boesch@gmail.com>
2021-09-12 18:04:45 +0100

    arrange peep history and display body/time/likes/user

--- src/loadPeeps.js
+++ src/loadPeeps.js
@@ -17,9 +17,18 @@ function loadPeepDB(data) {
   link.href = `#${data.id}`;
   const div = document.createElement('div');
   div.className = 'peep-list';
-  // div.innerHTML = formatPeepsHTML(data);
-  div.innerHTML = data.body;
+  div.innerHTML = arrangeData(data);
   link.appendChild(div);
   document.querySelector('#all-peeps').appendChild(link);
   peepsDB.push(data);
+}
+
+function arrangeData(data) {
+  return `<div class="inner-peep-div">
+  <h3 class="peep-body">${data.body}</h3>
+  <h6 class="peep-name">${data.user.handle}</h6>
+  <h6 class="peep-time">${data.updated_at}</h6>
+  <h6 class="peep-likes">Likes: ${data.likes.length}</h6>
+  </div>
+  <hr>`;
 }
