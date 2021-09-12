42c8e1db1fe35308df184ac9e14da24e2f82ee39
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-09-12 21:36:20 +0100

    Adding html elements in showPeeps()

--- public/js/interface.js
+++ public/js/interface.js
@@ -9,7 +9,27 @@ function showPeeps() {
     Peep.fetchAll().then(() => {
         Peep.all.forEach((peep) => {
             let peepDiv = document.createElement("div");
-            peepDiv.innerText = peep.body;
+            let peepBody = document.createElement("div");
+            let peepInfo = document.createElement("div");
+            let peepAuthor = document.createElement("span");
+            let peepDate = document.createElement("span");
+
+            peepBody.innerHTML = peep.body;
+            peepAuthor.innerHTML = `posted by ${peep.user.handle} `;
+            peepDate.innerHTML = `on ${peep.created_at}`;
+
+            peepBody.setAttribute("class", "peep-body");
+            peepInfo.setAttribute("class", "peep-info");
+            peepBody.setAttribute("class", "peep-body");
+            peepAuthor.setAttribute("class", "peep-author");
+            peepDate.setAttribute("class", "peep-date");
+
+            peepInfo.appendChild(peepAuthor);
+            peepInfo.appendChild(peepDate);
+
+            peepDiv.appendChild(peepBody);
+            peepDiv.appendChild(peepInfo);
+
             document.querySelector("body").appendChild(peepDiv);
         })
     })
