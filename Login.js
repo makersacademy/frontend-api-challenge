0e0323fda7b079d5c10719ff5833bf76cc98a852
Farya <farya.hsn@gmail.com>
2021-09-13 11:04:11 +0200

    here are some basic functions :)

---
+++ Login.js
@@ -0,0 +1,24 @@
+// const login = document.getElementById("login");
+
+// login.addEventListener("click", function () {
+//   loginUser(
+//     document.getElementById("usernameForLogin").value,
+//     document.getElementById("passwordForLogin").value
+//   );
+// });
+
+// function loginUser(username, password) {
+//   postData(
+//     "https://chitter-backend-api-v2.herokuapp.com/sessions",
+//     (data = {
+//       session: { handle: username, password: password },
+//     })
+//   )
+//     .then((response) => response.json())
+//     .then((data) => console.log(data));
+//   //   if (data.hasOwnProperty("errors")) {
+//   //     alert("login failed");
+//   //   } else {
+//   //     createSession(data, handle);
+//   //   }
+// }
---
+++ chitter.js
@@ -0,0 +1,10 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const view = new Peeps();
+//   this.view.viewData();
+//     for (let i = 0; i <= this.tweetbody.length - 1; i++) {
+//       let tweet = document.createElement("li");
+//       tweet.innerHTML = `"${this.tweetbody[i].body}"`;
+//       const tweetTable = viewallpeeps.querySelector("ul");
+//       tweetTable.appendChild(tweet);
+//     }
+// };
