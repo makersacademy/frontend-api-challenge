9c0af4c3b36c0bdf1936577af90a74e8cfa37923
AJOsmaston <dajoxx@gmail.com>
2021-11-07 20:43:48 +0000

    login and register added

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const displayPeeps = require('./lib/display/displayPeeps')
+const displayLogin = require('./lib/display/displayLogin')
 const getPeeps = require('./lib/fetches/getPeeps')
+const throwError = require('./lib/throwError')
+const createPeep = require('./lib/display/createPeep.js')
+const displayRegister = require('./lib/display/displayRegister')
 
 //get peeps then display peeps
 getPeeps(displayPeeps);
+
+//display login screen, with link to create new user
+displayLogin(
+  throwError, createPeep
+);
