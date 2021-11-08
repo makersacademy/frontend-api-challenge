9b7890465aec074e964852a2401c54278b3a6d39
luke <luke44@gmail.com>
2021-11-08 00:34:46 +0000

    User reg creates session

--- bundle.js
+++ bundle.js
@@ -17,15 +17,28 @@ var require_fetchPeeps = __commonJS({
 // newUser.js
 var require_newUser = __commonJS({
   "newUser.js"() {
-    updateServer = (element2) => {
+    updateServer = (element) => {
       fetch("https://chitter-backend-api-v2.herokuapp.com/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
-        body: JSON.stringify(element2)
+        body: JSON.stringify(element)
       }).then((response) => response.json()).then((data) => {
         console.log("Success:", data);
+      }).catch((error) => {
+        console.error("Error:", error);
+      });
+    };
+    createSession = (element) => {
+      fetch("https://chitter-backend-api-v2.herokuapp.com/sessions", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(element)
+      }).then((response) => response.json()).then((data) => {
+        console.log("Session key is: ", data["session_key"]);
       }).catch((error) => {
         console.error("Error:", error);
       });
@@ -34,8 +47,10 @@ var require_newUser = __commonJS({
     button.addEventListener("click", () => {
       handle = document.querySelector("#new-user-username").value;
       password = document.querySelector("#new-user-password").value;
-      element = { "user": { "handle": `${handle}`, "password": `${password}` } };
-      updateServer(element);
+      userElement = { "user": { "handle": `${handle}`, "password": `${password}` } };
+      updateServer(userElement);
+      sessionElement = { "session": { "handle": `${handle}`, "password": `${password}` } };
+      createSession(sessionElement);
     });
   }
 });
@@ -44,11 +59,11 @@ var require_newUser = __commonJS({
 var fetchPeeps2 = require_fetchPeeps();
 var newUser = require_newUser();
 fetchPeeps2((jsonData) => {
-  jsonData.forEach((element2) => createElement(element2));
+  jsonData.forEach((element) => createElement(element));
 });
-createElement = (element2) => {
+createElement = (element) => {
   newDiv = document.createElement("div");
-  newContent = document.createTextNode(`On ${element2["created_at"]}, ${element2["user"]["handle"]} posted: ${element2["body"]}`);
+  newContent = document.createTextNode(`On ${element["created_at"]}, ${element["user"]["handle"]} posted: ${element["body"]}`);
   newDiv.className = "post";
   idNumber = document.querySelectorAll(".post").length + 1;
   newDiv.id = "post-" + idNumber;
--- newUser.js
+++ newUser.js
@@ -15,11 +15,30 @@ updateServer = (element) => {
   });
 }
 
+createSession = (element) => {
+  fetch('https://chitter-backend-api-v2.herokuapp.com/sessions', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(element),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Session key is: ', data['session_key']);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
 const button = document.querySelector('#create-new-user')
 
 button.addEventListener('click', () => {
   handle = document.querySelector('#new-user-username').value;
   password = document.querySelector('#new-user-password').value;
-  element = {"user": { "handle": `${handle}`, "password": `${password}`}};
-  updateServer(element);
+  userElement = {"user": { "handle": `${handle}`, "password": `${password}`}};
+  updateServer(userElement);
+  sessionElement = {"session": { "handle": `${handle}`, "password": `${password}`}};
+  createSession(sessionElement);
 })
