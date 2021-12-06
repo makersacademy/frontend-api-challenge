d25b9afd934fbdbce40e4bd14196b2bd4ae4ae49
bmargerison <bmargerison@outlook.com>
2021-12-06 10:34:31 +0000

    refactor

--- bundle.js
+++ bundle.js
@@ -37,9 +37,22 @@
         let count = 1;
         fetch("https://chitter-backend-api-v2.herokuapp.com/peeps").then((response) => response.json()).then((data2) => {
           data2.forEach((peep) => {
-            const div = document.createElement("p");
-            div.innerText = peep.body;
+            const div = document.createElement("div");
+            div.className = "peeps";
+            const div1 = document.createElement("div");
+            const div2 = document.createElement("div");
+            const div3 = document.createElement("div");
+            div.className = "peeps";
             div.id = `peep-${count}`;
+            div1.className = "peep_body";
+            div1.innerText = peep.body;
+            div3.className = "liked by";
+            div3.innerText = `posted_by ${data2.user_id}`;
+            div2.className = "likes";
+            div2.innerText = data2.likes;
+            div.append(div1);
+            div.append(div2);
+            div.append(div3);
             document.body.append(div);
             count++;
           });
