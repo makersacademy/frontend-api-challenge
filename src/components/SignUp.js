7023f865b392ec0a002f0b89e25a78f35ad34832
j-cort <81960011+j-cort@users.noreply.github.com>
2022-01-26 10:41:08 +0000

    fix useEffect import in SignUp

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 import chitter from "../apis/chitter";
 
 const SignUp = ({ setShowSignIn, setAlert }) => {
