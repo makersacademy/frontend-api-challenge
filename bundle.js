7acff11db7006e476ab55dbf2081b020a00a0005
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-10 21:13:47 +0000

    Added pulling peeps back and displaying on page

--- bundle.js
+++ bundle.js
@@ -1,4 +1,74 @@
 (() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // lib/chitterModel.js
+  var require_chitterModel = __commonJS({
+    "lib/chitterModel.js"(exports, module) {
+      var ChitterModel2 = class {
+        constructor() {
+          this.peeps = [];
+        }
+        getPeeps() {
+          return this.peeps;
+        }
+        setPeeps(peeps) {
+          this.peeps = peeps;
+        }
+      };
+      module.exports = ChitterModel2;
+    }
+  });
+
+  // views/chitterView.js
+  var require_chitterView = __commonJS({
+    "views/chitterView.js"(exports, module) {
+      var ChitterView2 = class {
+        constructor(ChitterModel2, api2) {
+          this.model = ChitterModel2;
+          this.api = api2;
+          this.mainContainerEl = document.querySelector("#main-container");
+        }
+        displayPeeps() {
+          const peeps = this.model.getPeeps();
+          document.querySelectorAll(".peep").forEach((e) => e.remove());
+          peeps.forEach((peep) => {
+            const peepEl = document.createElement("div");
+            peepEl.innerText = peep.body;
+            peepEl.className = "peep";
+            this.mainContainerEl.append(peepEl);
+          });
+        }
+      };
+      module.exports = ChitterView2;
+    }
+  });
+
+  // lib/chitterApi.js
+  var require_chitterApi = __commonJS({
+    "lib/chitterApi.js"(exports, module) {
+      var ChitterApi = class {
+        loadPeeps(callback) {
+          fetch("https://chitter-backend-api-v2.herokuapp.com/peeps").then((response) => response.json()).then((data) => {
+            callback(data);
+          });
+        }
+      };
+      module.exports = ChitterApi;
+    }
+  });
+
   // index.js
-  console.log("Testing this is working");
+  var ChitterModel = require_chitterModel();
+  var ChitterView = require_chitterView();
+  var ChittersApi = require_chitterApi();
+  var api = new ChittersApi();
+  var model = new ChitterModel();
+  var view = new ChitterView(model, api);
+  api.loadPeeps((peeps) => {
+    model.setPeeps(peeps);
+    view.displayPeeps();
+  });
 })();
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Testing this is working');
+const ChitterModel = require('./lib/chitterModel');
+const ChitterView = require('./views/chitterView'); 
+const ChittersApi = require("./lib/chitterApi");
+
+const api = new ChittersApi()
+const model = new ChitterModel()
+const view = new ChitterView(model, api);
+
+api.loadPeeps((peeps) => {
+  model.setPeeps(peeps);
+  view.displayPeeps();
+});
