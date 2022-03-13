c61f28f2a2ea6b3ad8778d5e5c1c080a2fcba8c2
Jack <winterschladen.j@gmail.com>
2022-03-13 12:38:07 +0000

    Initial files set up

---
+++ README.md
@@ -0,0 +1,4 @@
+As a user
+So that I can keep up to date with my friends
+I'd like to see a list of all Peeps
+
---
+++ peepsView.test.js
@@ -0,0 +1,8 @@
+describe(PeepsView, () => {
+  
+  describe('#displayPeeps', () => {
+    it('displays a list of peeps', () => {
+      
+    })
+  })
+})
