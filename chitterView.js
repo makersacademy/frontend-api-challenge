f9f8f4948b984daaa86d0dff434fd3b5e9f26822
arhussain1 <a.r.hussain@hotmail.com>
2022-04-10 19:08:12 +0100

    Can now add new peeps dynamically by click Add peep button

--- chitterView.js
+++ chitterView.js
@@ -4,6 +4,18 @@ class ChitterView {
   constructor(model) {
     this.model = model
 
+    this.buttonEl = document.querySelector('#add-peep-button')
+    this.inputEl = document.querySelector('#peep-input')
+
+    this.buttonEl.addEventListener('click', () => {
+      if (this.inputEl) {this.addNewPeep(this.inputEl.value)};
+
+      this.displayPeeps()
+    })
+  }
+
+  addNewPeep(peep) {
+    this.model.addPeep(peep);
   }
 
   displayPeeps() {
