c5bf4e14948b9f722b18ed87c874a7420e6d89ad
Luke Storey <lukemstorey@gmail.com>
2022-06-04 10:44:47 +0100

    Implement user sign up

--- chitterView.js
+++ chitterView.js
@@ -3,6 +3,27 @@ class ChitterView {
     this.model = model;
     this.api = api;
     this.mainContainerEl = document.querySelector("#main-container");
+    this.createAccountBtn = document.querySelector("#create-account-btn");
+
+    this.createAccountBtn.addEventListener("click", () => {
+      this.showAndHide("#create-account-container");
+    });
+
+    this.createHandleInput = document.querySelector("#create-account-handle");
+
+    this.createPasswordInput = document.querySelector(
+      "#create-account-password"
+    );
+
+    this.noticeEl = document.querySelector("#notice");
+
+    this.createAccountSubmit = document.querySelector("#create-account-submit");
+    this.createAccountSubmit.addEventListener("click", () => {
+      this.createAccount(
+        this.createHandleInput.value,
+        this.createPasswordInput.value
+      );
+    });
   }
 
   async displayPeepsFromApi() {
@@ -19,11 +40,30 @@ class ChitterView {
     });
   }
 
+  async createAccount(handle, password) {
+    if (handle && password) {
+      const user = await this.api.createUser(handle, password);
+
+      this.noticeEl.innerText = `Thanks ${user.handle} your account has been created`;
+
+      this.showAndHide("#create-account-container");
+    }
+  }
+
   addPeepEl(peepBody) {
     const newDiv = document.createElement("div");
     newDiv.innerText = peepBody;
     newDiv.setAttribute("class", "peep");
     this.mainContainerEl.append(newDiv);
+  }
+
+  showAndHide(element) {
+    var x = document.querySelector(element);
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
   }
 }
 
--- chitterView.test.js
+++ chitterView.test.js
@@ -16,19 +16,41 @@ describe("ChitterView", () => {
       fetchPeeps: () => {
         return [{ body: "this is a peep from the mock api" }];
       },
+
+      createUser: () => {
+        return { handle: "luke", id: 1 };
+      },
     };
 
     view = new ChitterView(model, mockApi);
   });
 
-  describe("displayPeepsFromApi", () => {
-    it("should display peeps from server on page", async () => {
-      await view.displayPeepsFromApi();
+  it("should display peeps from server on page", async () => {
+    await view.displayPeepsFromApi();
 
-      expect(document.querySelectorAll("div.peep").length).toBe(1);
-      expect(document.querySelectorAll("div.peep")[0].innerText).toBe(
-        "this is a peep from the mock api"
+    expect(document.querySelectorAll("div.peep").length).toBe(1);
+    expect(document.querySelectorAll("div.peep")[0].innerText).toBe(
+      "this is a peep from the mock api"
+    );
+  });
+
+  it("should allow the user to login", async () => {
+    const createAccountBtn = document.querySelector("#create-account-btn");
+    createAccountBtn.click();
+
+    const handleInput = document.querySelector("#create-account-handle");
+    handleInput.value = "luke";
+
+    const passwordInput = document.querySelector("#create-account-password");
+    passwordInput.value = "password123";
+
+    const createSubmitBtn = document.querySelector("#create-account-submit");
+    createSubmitBtn.click();
+
+    setTimeout(() => {
+      expect(document.querySelector("#notice").innerText).toEqual(
+        "Thanks luke your account has been created"
       );
-    });
+    }, 0);
   });
 });
