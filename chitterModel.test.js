e87a84275098aa6cd883e591f9b8c915542223bf
Russell <russellmorbey@hotmail.co.uk>
2022-06-05 17:29:51 +0100

    First commit. Project start.

---
+++ chitterModel.test.js
@@ -0,0 +1,1 @@
+const ChitterModel = require('./chitterModel');
---
+++ index.js
@@ -0,0 +1,3 @@
+const ChitterModel = require('./chitterModel');
+const ChitterApi = require('./chitterApi');
+const ChitterView = require('./chitterView')
