6f2a068c64afed0992ffc182cd9460656294a808
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-05 15:59:31 +0100

    re-considering plan of site and removed model as believe it is reduntant

--- chitterApi.test.js
+++ chitterApi.test.js
@@ -6,4 +6,4 @@ const ChitterApi= require('./chitterApi');
 
 
 
-expect git sttoHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps")
+expect toHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps")
