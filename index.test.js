5ddc87968e98605c04402e052a08899f12aaeb1b
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-05 08:04:22 +0100

    high level imagined structure added to first test

---
+++ index.test.js
@@ -0,0 +1,12 @@
+//write the code I wish I had
+
+
+//I wish I had something that accessed the server
+const accessServer = require('./accessServer');
+// I wish I had something that got all the peepr
+const getAllPeeps = require('./getAllPeeps');
+// I wish I had something that put the peeps on the screen
+const putPeepsOnScreen = require('./putPeepsOnScreen');
+
+//and I need a subject
+const subject = require('./index');
