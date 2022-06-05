0942ea3385bc4af2f4d79bc7299a04e02a72bf48
Yaroslava Yates <yaroslava.yates@gmail.com>
2022-06-05 18:13:00 +0100

    class Api and tests

--- apiChitter.js
+++ apiChitter.js
@@ -1,1 +1,28 @@
+class ApiChitter {
+  constructor(api = 'https://chitter-backend-api-v2.herokuapp.com/peeps') {
+    this.api = api;
+  }
+
+  loadPeeps(callback) {
+    fetch(this.api)
+      .then((response) => response.json())
+      .then((data) => callback(data));
+  }
+
+  createPeep(body, handle, callback) {
+    fetch('https://chitter-backend-api-v2.herokuapp.com/users', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        user: {
+          body: body,
+          handle: handle
+        },
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => callback(data));
+  }
+}
+
 module.exports = ApiChitter;
--- index.js
+++ index.js
@@ -6,4 +6,9 @@ const api = new ApiChitter();
 const model = new ChitterModel();
 const view = new ChitterView(model, api);
 
+api.loadPeeps((peep) => {
+  model.addPeep(peep);
+  view.displayPeeps();
+});
+
 console.log('Hello!');
