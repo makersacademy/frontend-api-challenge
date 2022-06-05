33a99d812095b05277250aacbbcef241dc48dd07
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-05 09:47:02 +0100

    initial setup test written

--- index.test.js
+++ index.test.js
@@ -1,12 +1,41 @@
-//write the code I wish I had
+/**
+ * @jest-environment jsdom
+ */
 
+ const fs = require('fs');
+
+//write the code I wish I had
+jest.mock('./accessServer');
+jest.mock('./getAllPeeps');
+jest.mock('./putPeepsOnScreen');
 
 //I wish I had something that accessed the server
-const accessServer = require('./accessServer');
+const ChitterApi = require('./chitterApi');
 // I wish I had something that got all the peepr
-const getAllPeeps = require('./getAllPeeps');
+const ChitterModel = require('./chitterModel');
 // I wish I had something that put the peeps on the screen
-const putPeepsOnScreen = require('./putPeepsOnScreen');
+const PeepsView = require('./PeepsView');
 
 //and I need a subject
 const subject = require('./index');
+
+
+
+describe('index', () => {
+
+  beforeEach(() => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+  });
+
+  it('displays all current peeps', () => {
+    const chitterApi = new ChitterApi();
+    const chitterModel = new ChitterModel(chitterApi);
+    const peepsView = new PeepsView(chitterModel);
+
+    peepsView.all();
+
+    expect(document.querySelectorAll('.peeps')).toBeGreaterThan(1);
+  })
+})
+
+// so what I am trying to do here is set up a test that helps me to start in line with what Justin Searls was talking about
