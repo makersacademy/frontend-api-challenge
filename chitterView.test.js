6e26c752218cd0ffb23c18e7cd8b41f74647cb59
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-06-05 13:05:53 +0100

    unsuccessfully added session test, com'd out

--- chitterView.test.js
+++ chitterView.test.js
@@ -2,12 +2,19 @@
  * @jest-environment jsdom
  */
 
+require('jest-fetch-mock').enableMocks();
+
 const fs = require('fs');
 const ChitterApi = require('./chitterApi');
 const ChitterModel = require('./chitterModel');
 const ChitterView = require('./chitterView');
 
 describe('ChitterView class', () => {
+
+  beforeEach(() => {
+    fetch.resetMocks();
+  })
+
   it('Displays peeps', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
 
@@ -23,7 +30,7 @@ describe('ChitterView class', () => {
       "likes":[]
     }];
 
-    const api = new ChitterApi
+    const api = new ChitterApi;
     const model = new ChitterModel(testPeep);
     const view = new ChitterView(model, api);
 
@@ -34,4 +41,25 @@ describe('ChitterView class', () => {
     expect(displayedPeeps[0].innerText).toEqual("this is a test");
   });
 
+  // the mocking in this doesn't work for some reason.
+  
+  // it('creates new sessions', () => {
+  //   fetch.mockResponseOnce(JSON.stringify({
+  //     "user_id":12345,
+  //     "session_key":"_3b_65_WEjfcW0unkmN9uVtIMa24f"
+  //   }));
+
+  //   document.body.innerHTML = fs.readFileSync('./index.html');
+
+  //   const api = new ChitterApi;
+  //   const model = new ChitterModel;
+  //   const view = new ChitterView(model, api);
+
+  //   document.querySelector('#handle').value = "testHandle";
+  //   document.querySelector('#password').value = "testPassword";
+  //   document.querySelector('#login-button').click();
+
+  //   expect(view.userID).toEqual(12345);
+  //   expect(view.sessionKey).toEqual("_3b_65_WEjfcW0unkmN9uVtIMa24f")
+  // })
 });
