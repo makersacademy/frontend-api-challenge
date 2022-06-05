9d4f618a4e9275e113f9e12794f47719ac509659
Russell <russellmorbey@hotmail.co.uk>
2022-06-05 18:06:25 +0100

    ChitterModel tests passed.

--- chitterModel.js
+++ chitterModel.js
@@ -1,3 +1,24 @@
-
-
+class ChitterModel {
+  constructor() {
+    this.peeps = [];
+  }
+ 
+  setPeeps(peeps){
+    this.reset();
+    peeps.forEach((peep) => this.addPeep(peep));
+  }
+ 
+  getPeeps() {
+    return this.peeps;
+  }
+ 
+  addPeep(peep) {
+    this.peeps.push(peep)
+  }
+ 
+  reset() {
+    this.peeps = [];
+  }
+ 
+ };
 module.exports = ChitterModel;
