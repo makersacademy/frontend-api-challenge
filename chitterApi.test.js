267e4cdb26782577b1dfc5e56533c9b6e619285f
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-05 16:50:03 +0100

    written failing test with mocks

--- chitterApi.test.js
+++ chitterApi.test.js
@@ -1,9 +1,33 @@
 const ChitterApi= require('./chitterApi');
 
+require('jest-fetch-mock').enableMocks();
+
+beforeEach(() => {
+  fetch.resetMocks();
+});
+
+describe('ChitterApi', () => {
+  describe('.loadPeeps', () => {
+    it('calls fetch and retrieves the peeps from the server', async () => {
+      //given
+      const chitterApi = new ChitterApi;
+      fetch.mockResponseOnce(JSON.stringify(['Dr sillyboots new silly tweet']));
+      let result;
+
+      //when
+      await chitterApi.loadPeeps((peeps) => {
+        result = peeps;
+      });
+
+      //then
+      expect(result.length).toEqual(1);
+      expect(result[0]).toEqual('Dr sillyboots new silly tweet')
+    })
+  })
+})
 
 
 
 
 
-
-expect toHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps")
+// expect toHaveBeenCalledWith("https://chitter-backend-api-v2.herokuapp.com/peeps")
