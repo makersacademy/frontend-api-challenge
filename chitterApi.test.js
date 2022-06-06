fb24e1b40095b5ab2a424841715a463011d301d7
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 <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>
2022-06-06 10:56:17 +0100

    adding the apis

---
+++ chitterApi.test.js
@@ -0,0 +1,17 @@
+// const ChitterApi = require('./chitterApi')
+
+// require('jest-fetch-mock').enableFetchMocks()
+
+// describe('loadChits', () => {
+//   it('fetches chits from the server', (done) => {
+//     const api = new ChitterApi();
+
+//     fetch.mockResponseOnce(async (request) => {
+//       return.JSON.stringify(['hello']);
+//     })
+
+//     api.loadChits((response) => {
+//       expect(fetch.mock.calls.length).toEqual(1)
+//     })
+//   })
+// })
