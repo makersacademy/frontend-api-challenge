0716c8b3f813cf4813ee5250d0e40d3b3f1cf746
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-03 20:31:44 +0100

    display peeps tested

--- peepsView.js
+++ peepsView.js
@@ -1,5 +1,31 @@
 class PeepsView {
+  constructor(model, api) {
+    this.model = model;
+    this.api = api;
+    this.mainContainerEl = document.querySelector('#main-container');
+  }
 
+  displayClear() {
+    const peeps = document.querySelectorAll('div.peep');
+    peeps.forEach(peep => peep.remove());
+  }
+
+  displayPeeps() {
+    this.displayClear()
+    this.model.getPeeps().forEach((peep) => {
+      const divEl = document.createElement('div');
+      divEl.className = 'peep';
+      divEl.textContent = peep.body;
+      this.mainContainerEl.append(divEl);
+    })
+  }
+
+  displayFromApi() {
+    this.api.loadPeeps((peeps) => {
+      this.model.setPeeps(peeps);
+      this.displayPeeps();
+    })
+  }
 }
 
 module.exports = PeepsView;
--- peepsView.test.js
+++ peepsView.test.js
@@ -9,11 +9,48 @@ const PeepsModel = require('./peepsModel');
 describe('PeepsView class', () => {
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('./index.html');
+    model = new PeepsModel;
   })
 
-  it('', () => {
-    // stuff
+  it('displays peeps', () => {
+    const peepsView = new PeepsView(model);
+    model.setPeeps([{ body: 'test peep' }]);
+    peepsView.displayPeeps();
+    expect(document.querySelector('div.peep').textContent).toEqual('test peep');
   })
 
+
+  // it('clears peeps', () => {
+  //   const peepsView = new PeepsView(model)
+  //   model.setPeeps(['test peep']);
+  //   peepsView.displayClear()
+  //   expect(document.querySelectorAll('div.peep').length).toBe(0)
+  // })
+
+  // it('displayPeeps method', () => {
+  //   const peep = [
+  //     {
+  //       "id": 3,
+  //       "body": "my first peep :)",
+  //       "created_at": "2018-06-23T13:21:23.317Z",
+  //       "updated_at": "2018-06-23T13:21:23.317Z",
+  //       "user": {
+  //         "id": 1,
+  //         "handle": "kay"
+  //       },
+  //       "likes": [{
+  //         "user": {
+  //           "id": 1,
+  //           "handle": "kay"
+  //         }
+  //       }]
+  //     }
+  //   ]
+
+  //   const api = { loadPeeps: (callback) => callback(peep) };
+  //   const peepsView = new PeepsView(model, api);
+  //   peepsView.displayFromApi();
+  //   expect(document.querySelector('div.peep')).toBe('my first peep');
+  // })
 
 })
