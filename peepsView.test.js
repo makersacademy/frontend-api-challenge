a76ae3532f09d8336d1216fad59bec7bf7d67010
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-03 20:39:44 +0100

    test drive displayFromApi method

--- peepsView.test.js
+++ peepsView.test.js
@@ -19,7 +19,7 @@ describe('PeepsView class', () => {
     expect(document.querySelector('div.peep').textContent).toEqual('test peep');
   })
 
-  it('clears peeps', () => {
+  it('displayClear method', () => {
     const peepsView = new PeepsView(model)
     model.setPeeps([{ body: 'test peep' }]);
     peepsView.displayPeeps();
@@ -27,30 +27,30 @@ describe('PeepsView class', () => {
     expect(document.querySelectorAll('div.peep').length).toBe(0)
   })
 
-  // it('displayPeeps method', () => {
-  //   const peep = [
-  //     {
-  //       "id": 3,
-  //       "body": "my first peep :)",
-  //       "created_at": "2018-06-23T13:21:23.317Z",
-  //       "updated_at": "2018-06-23T13:21:23.317Z",
-  //       "user": {
-  //         "id": 1,
-  //         "handle": "kay"
-  //       },
-  //       "likes": [{
-  //         "user": {
-  //           "id": 1,
-  //           "handle": "kay"
-  //         }
-  //       }]
-  //     }
-  //   ]
+  it('displayFromApi method', () => {
+    const peep = [
+      {
+        "id": 3,
+        "body": "my first peep :)",
+        "created_at": "2018-06-23T13:21:23.317Z",
+        "updated_at": "2018-06-23T13:21:23.317Z",
+        "user": {
+          "id": 1,
+          "handle": "kay"
+        },
+        "likes": [{
+          "user": {
+            "id": 1,
+            "handle": "kay"
+          }
+        }]
+      }
+    ]
 
-  //   const api = { loadPeeps: (callback) => callback(peep) };
-  //   const peepsView = new PeepsView(model, api);
-  //   peepsView.displayFromApi();
-  //   expect(document.querySelector('div.peep')).toBe('my first peep');
-  // })
+    const api = { loadPeeps: (callback) => callback(peep) };
+    const peepsView = new PeepsView(model, api);
+    peepsView.displayFromApi();
+    expect(document.querySelector('div.peep').textContent).toBe('my first peep :)');
+  })
 
 })
