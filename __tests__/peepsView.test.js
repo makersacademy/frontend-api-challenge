3c5fcfc933ec17637f6ea4dda13b3bd103d1636f
nickwlong <nickwlong@gmail.com>
2022-09-16 14:26:44 +0100

    Test driven a button

---
+++ __tests__/peepsView.test.js
@@ -0,0 +1,27 @@
+/**
+ * @jest-environment jsdom
+*/
+
+const fs = require("fs");
+const Api = require("../src/api");
+const PeepModel = require("../src/model/user");
+const PeepsView = require("../src/views/peepsView");
+
+
+describe('Clicking create an account button makes form visible', function() {
+  test('', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+  
+    const model = new PeepModel
+    const api = new Api
+    const view = new PeepsView(model, api)
+  
+    expect(document.getElementById('container-createUser').style.visibility).toBe('hidden')
+  
+    const button = document.getElementById('showCreateUser'); //collects the button
+    button.click() //clicks the button
+  
+    expect(document.getElementById('container-createUser').style.visibility).toBe('visible')  
+  })
+
+})
