ea58ff87c00456837a5d32307d3b8619bcb82aa3
mirandaweston <mweston105@yahoo.co.uk>
2023-03-04 19:23:58 +0000

    Adding PeepModel class and test suite: passes 'getPeeps()' and 'addPeep(peep)' methods

---
+++ src/peepModel.js
@@ -0,0 +1,15 @@
+class PeepsModel {
+  constructor() {
+    this.peeps = [];
+  }
+
+  getPeeps() {
+    return this.peeps;
+  }
+
+  addPeep(peep) {
+    this.peeps.push(peep);
+  }
+}
+
+module.exports = PeepsModel;
