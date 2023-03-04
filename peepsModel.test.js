471039610797b296a8e517a44eb02b0027b8bc73
Hayley Dobbs <iamdobbs@icloud.com>
2023-03-04 16:27:12 +0000

    Refactored to use 'model' for const name

--- peepsModel.test.js
+++ peepsModel.test.js
@@ -1,27 +1,27 @@
 const PeepsModel = require ('./peepsModel')
 
 describe('PeepsModel', () => {
-  let peep;
+  let model;
 
   beforeEach(() => {
-    peep = new PeepsModel();
+    model = new PeepsModel();
   });
 
     it('constructs with an empty peeps array', () => {
-      expect(peep.peeps).toEqual([]);
+      expect(model.peeps).toEqual([]);
     });
 
     it('can add a peep', () => {
-      peep.addPeep('This is a peep')
-      expect(peep.peeps).toContain('This is a peep');
-      expect(peep.getPeeps()).toEqual(['This is a peep']);
+      model.addPeep('This is a peep')
+      expect(model.peeps).toContain('This is a peep');
+      expect(model.getPeeps()).toEqual(['This is a peep']);
     });
 
     it('can add a multiple peeps', () => {
-      peep.addPeep('This is a peep')
-      peep.addPeep('This is another peep')
-      expect(peep.peeps).toContain('This is a peep');
-      expect(peep.peeps).toContain('This is another peep');
-      expect(peep.getPeeps()).toEqual(['This is a peep', 'This is another peep']);
+      model.addPeep('This is a peep')
+      model.addPeep('This is another peep')
+      expect(model.peeps).toContain('This is a peep');
+      expect(model.peeps).toContain('This is another peep');
+      expect(model.getPeeps()).toEqual(['This is a peep', 'This is another peep']);
     });
 })
