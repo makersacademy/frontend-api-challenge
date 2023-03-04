1801ee15197459406026fd2cac792905d1f915c1
Hayley Dobbs <iamdobbs@icloud.com>
2023-03-04 15:46:56 +0000

    Created #addPeep and #getPeeps. Tests passed for both functions.

--- peepsModel.js
+++ peepsModel.js
@@ -3,6 +3,14 @@ class PeepsModel {
   constructor () {
     this.peeps = [];
   }
+
+  addPeep (peep) {
+    this.peeps.push(peep);
+  }
+
+  getPeeps () {
+    return this.peeps;
+  }
 }
 
 module.exports = PeepsModel;
--- peepsModel.test.js
+++ peepsModel.test.js
@@ -1,8 +1,19 @@
 const PeepsModel = require ('./peepsModel')
 
 describe('PeepsModel', () => {
-  it('constructs with an empty peeps array', () => {
-    const peep = new PeepsModel;
-    expect(peep.peeps).toEqual([]);
+  let peep;
+
+  beforeEach(() => {
+    peep = new PeepsModel();
   });
+
+    it('constructs with an empty peeps array', () => {
+      expect(peep.peeps).toEqual([]);
+    });
+
+    it('can add a peep', () => {
+      peep.addPeep('This is a peep')
+      expect(peep.peeps).toContain('This is a peep');
+      expect(peep.getPeeps()).toEqual(['This is a peep']);
+    });
 })
