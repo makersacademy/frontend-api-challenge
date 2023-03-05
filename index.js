7cba84e5a99e887e52de8d0bdbb4aa00f13579f8
mirandaweston <mweston105@yahoo.co.uk>
2023-03-05 01:09:07 +0000

    Updating main file to show peeps displaying

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Chitter is up and running');
+// console.log('Chitter is up and running');
+
+const PeepsModel = require('./src/peepsModel');
+const PeepsView = require('./src/peepsView');
+
+let model = new PeepsModel;
+
+console.log(model.getPeeps());
+
+model.addPeep('example peep');
+
+let view = new PeepsView(model);
+
+view.displayPeeps();
