3237c5e82f552acb0ca4991f6dd05cc56eed86df
sameera sood <sameerasood@gmail.com>
2023-03-05 02:07:48 +0000

    Test drive implementation of displayPeeps function

--- _tests_/chitterView.test.js
+++ _tests_/chitterView.test.js
@@ -13,14 +13,23 @@ describe("ChitterView", () => {
   beforeEach(() => {
     document.body.innerHTML = fs.readFileSync("./index.html");
     ChitterClient.mockClear();
+  });
 
+  it("displays multiple peeps", () => {
     const model = new ChitterModel();
     const mockClient = new ChitterClient();
     const view = new ChitterView(model, mockClient);
+    model.addPeep("Test peep");
+    model.addPeep("Test peep 2");
+    view.displayPeeps();
+    expect(document.querySelectorAll("div.peep").length).toEqual(2);
   });
 
-  it("displays peeps from the API", () => {
-    view.displayPeeps();
+  it.only("displays peeps from the API", () => {
+    const model = new ChitterModel();
+    const mockClient = new ChitterClient();
+    const view = new ChitterView(model, mockClient);
+    view.displayPeepsFromApi();
     const peeps = document.querySelectorAll(".peep");
   });
 });
--- src/chitterView.js
+++ src/chitterView.js
@@ -1,20 +1,30 @@
 class ChitterView {
-  constructor() {
+  constructor(model, client) {
     this.model = model;
     this.client = client;
     this.mainContainerEl = document.querySelector("#main-container");
   }
+
+  displayPeeps() {
+    document.querySelectorAll(".peep").forEach((element) => {
+      element.remove();
+    });
+
+    const peeps = this.model.getPeeps();
+
+    peeps.forEach((peep) => {
+      const newPeepEl = document.createElement("div");
+      newPeepEl.textContent = peep;
+      newPeepEl.className = "peep";
+      this.mainContainerEl.append(newPeepEl);
+    });
+  }
+
+  async displayPeepsFromApi() {
+    const peeps = await this.client.loadPeeps();
+    await this.model.setPeeps(peeps);
+    this.displayPeeps();
+  }
 }
 
 module.exports = ChitterView;
-
-constructor(model, client);
-this.model = model;
-this.client = client;
-
-this.mainContainerEl = document.querySelector("#main-container");
-
-this.mainContainer(() => {
-  const allPeeps = document.querySelector("main-container".body);
-  this.addPeeps(allPeeps);
-});
