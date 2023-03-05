5aeb9d0104158c7d1848c55796bd80280cc95cf6
Hayley Dobbs <iamdobbs@icloud.com>
2023-03-05 13:42:37 +0000

    Expanded on fetch/load test to include times called and beforeEach, resetMocks function

--- peepsClient.test.js
+++ peepsClient.test.js
@@ -2,16 +2,21 @@ const PeepsClient = require('./peepsClient');
 
 require('jest-fetch-mock').enableMocks();
 
-describe('PeepsClient class', () => {
-  it('calls fetch and loads peeps', (done) => {
-    const client = new PeepsClient();
-    fetch.mockResponseOnce(JSON.stringify({
-      peep: 'This is a mock peep',
-    }));
-
-    client.loadPeeps((returnedDataFromApi) => {
-      expect(returnedDataFromApi.peep).toBe('This is a mock peep');
-      done();
-    });
-  });  
+beforeEach(() => {
+  fetch.resetMocks();
 });
+
+  describe('PeepsClient class', () => {
+    it('calls fetch and loads peeps', (done) => {
+      const client = new PeepsClient();
+      fetch.mockResponseOnce(JSON.stringify({
+        peep: 'This is a mock peep',
+      }));
+
+      client.loadPeeps((returnedDataFromApi) => {
+        expect(returnedDataFromApi.peep).toBe('This is a mock peep');
+        expect(fetch).toHaveBeenCalledTimes(1);
+        done();
+      });
+    });  
+  })
