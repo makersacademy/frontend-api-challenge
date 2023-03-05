8080df15db32df5f8d746fc4c185f11cb5606e72
mirandaweston <mweston105@yahoo.co.uk>
2023-03-05 23:16:56 +0000

    Displaying peeps in reverse chronological order

--- src/peepsView.js
+++ src/peepsView.js
@@ -16,10 +16,19 @@ class PeepsView {
     });
 
     const peeps = this.model.getPeeps();
+    const sortedPeeps = peeps.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
-    peeps.forEach(peep => {
+    sortedPeeps.forEach(peep => {
       const peepE1 = document.createElement('div');
-      peepE1.textContent = peep;
+      peepE1.innerHTML = `
+        <div>id: ${peep.id}</div>
+        <div>Body: ${peep.body}</div>
+        <div>Created at: ${peep.created_at}</div>
+        <div>Updated at: ${peep.updated_at}</div>
+        <div>User Id: ${peep.user.id}</div>
+        <div>User handle: ${peep.user.handle}</div>
+        <div>Likes: ${peep.likes.map(like => like.user.handle).join(', ')}</div>
+      `;
       peepE1.className = 'peep';
       this.mainContainerE1.append(peepE1);
     });
@@ -29,7 +38,7 @@ class PeepsView {
     this.model.addPeep(newPeep);
     this.displayPeeps();
   }
-  
+
   displayPeepsFromApi() {
     return this.client.loadPeeps().then((peeps) => {
       this.model.setPeeps(peeps);
