1c3d9c35c98d76078a3603ec82e1e85a5bcc04b1
terryhycheng <terryhycheng@gmail.com>
2023-03-06 02:59:49 +0000

    Updated OpenAi prompt

--- src/utils/openAiHelper.ts
+++ src/utils/openAiHelper.ts
@@ -15,6 +15,7 @@ export const openAiClient = async (content: string, qty: number) => {
       max_tokens: 300,
     });
     const data = res.data as OpenApiRes;
+    console.log(data.choices[0]);
     return covertToObject(data.choices[0].text);
   } catch (error) {
     console.log(error);
@@ -22,10 +23,10 @@ export const openAiClient = async (content: string, qty: number) => {
 };
 
 const generatePrompt = (content: string, qty: number) => {
-  return `I want to create fake comments for my Twitter clone project which is called Chitter. In Chitter, 'peep' equals 'tweet' in Twitter. Please create ${qty} fake user names and fake comments to put under this peep: "${content}". Do not use comma, {, }, ', ", or : inside the comments.
+  return `I want to create fake comments for my Twitter clone project which is called Chitter. In Chitter, 'peep' equals 'tweet' in Twitter. Please create ${qty} fake user names and fake comments to put under this peep: "${content}". Do not use the following inside the comments: , or { or } or ' or " or : 
 
 Example:
-{username: 'terry', content: 'put the content here'}
+{username: "terry", content: "put the content here"}
 Suggestions:`;
 };
 
@@ -35,10 +36,9 @@ const covertToObject = (commentString: string): CommentData[] => {
     .filter((str) => str.length > 0)
     .map((str) => {
       const values = str.split(", ");
-      const username = values[0].split(": ")[1].replace(/'/g, "");
+      const username = values[0].split(": ")[1].replace(/"/g, "");
       const content = values[1]
         .split(": ")[1]
-        .replace(/'/g, "")
         .replace(/"/g, "")
         .replace("}", "");
       return { id: crypto.randomUUID(), username, content };
